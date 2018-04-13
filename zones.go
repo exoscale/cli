11e012599948507c4d96d143d323f05cb5be6267
Yoan Blanc <yoan.blanc@exoscale.ch>
2018-04-13 11:06:57 +0200

    zones: implements Gettable
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

--- zones.go
+++ zones.go
@@ -1,8 +1,35 @@
 package egoscale
 
 import (
+	"context"
 	"fmt"
+
+	"github.com/jinzhu/copier"
 )
+
+// Get fetches the given zone by ID or Name
+func (zone *Zone) Get(ctx context.Context, client *Client) error {
+	if zone.ID == "" && zone.Name == "" {
+		return fmt.Errorf("A Zone may only be obtained using ID or Name")
+	}
+
+	zones, err := client.ListWithContext(ctx, zone)
+	if err != nil {
+		return err
+	}
+
+	count := len(zones)
+	if count == 0 {
+		return &ErrorResponse{
+			ErrorCode: ParamError,
+			ErrorText: fmt.Sprintf("Zone not found. id: %q, name: %q", zone.ID, zone.Name),
+		}
+	} else if count > 1 {
+		return fmt.Errorf("More than one Zone was found. Query: id: %s", zone.ID)
+	}
+
+	return copier.Copy(zone, zones[0])
+}
 
 // ListRequest builds the ListZones request
 func (zone *Zone) ListRequest() (ListCommand, error) {
