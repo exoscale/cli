5b6ddff5161349c65892772f522e6844ffe75ba3
Yoan Blanc <yoan.blanc@exoscale.ch>
2018-04-20 16:27:21 +0200

    keypairs: add doc
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

--- keypairs_type.go
+++ keypairs_type.go
@@ -16,10 +16,10 @@ type SSHKeyPair struct {
 //
 // CloudStack API: http://cloudstack.apache.org/api/apidocs-4.10/apis/createSSHKeyPair.html
 type CreateSSHKeyPair struct {
-	Name      string `json:"name"`
-	Account   string `json:"account,omitempty"`
-	DomainID  string `json:"domainid,omitempty"`
-	ProjectID string `json:"projectid,omitempty"`
+	Name      string `json:"name" doc:"Name of the keypair"`
+	Account   string `json:"account,omitempty" doc:"an optional account for the ssh key. Must be used with domainId."`
+	DomainID  string `json:"domainid,omitempty" doc:"an optional domainId for the ssh key. If the account parameter is used, domainId must also be used."`
+	ProjectID string `json:"projectid,omitempty" doc:"an optional project for the ssh key"`
 }
 
 // CreateSSHKeyPairResponse represents the creation of an SSH Key Pair
@@ -31,21 +31,21 @@ type CreateSSHKeyPairResponse struct {
 //
 // CloudStack API: http://cloudstack.apache.org/api/apidocs-4.10/apis/deleteSSHKeyPair.html
 type DeleteSSHKeyPair struct {
-	Name      string `json:"name"`
-	Account   string `json:"account,omitempty"`
-	DomainID  string `json:"domainid,omitempty"`
-	ProjectID string `json:"projectid,omitempty"`
+	Name      string `json:"name" doc:"Name of the keypair"`
+	Account   string `json:"account,omitempty" doc:"the account associated with the keypair. Must be used with the domainId parameter."`
+	DomainID  string `json:"domainid,omitempty" doc:"the domain ID associated with the keypair"`
+	ProjectID string `json:"projectid,omitempty" doc:"the project associated with keypair"`
 }
 
 // RegisterSSHKeyPair represents a new registration of a public key in a keypair
 //
 // CloudStack API: http://cloudstack.apache.org/api/apidocs-4.10/apis/registerSSHKeyPair.html
 type RegisterSSHKeyPair struct {
-	Name      string `json:"name"`
-	PublicKey string `json:"publickey"`
-	Account   string `json:"account,omitempty"`
-	DomainID  string `json:"domainid,omitempty"`
-	ProjectID string `json:"projectid,omitempty"`
+	Name      string `json:"name" doc:"Name of the keypair"`
+	PublicKey string `json:"publickey" doc:"Public key material of the keypair"`
+	Account   string `json:"account,omitempty" doc:"an optional account for the ssh key. Must be used with domainId."`
+	DomainID  string `json:"domainid,omitempty" doc:"an optional domainId for the ssh key. If the account parameter is used, domainId must also be used."`
+	ProjectID string `json:"projectid,omitempty" doc:"an optional project for the ssh key"`
 }
 
 // RegisterSSHKeyPairResponse represents the creation of an SSH Key Pair
@@ -57,16 +57,16 @@ type RegisterSSHKeyPairResponse struct {
 //
 // CloudStack API: http://cloudstack.apache.org/api/apidocs-4.10/apis/listSSHKeyPairs.html
 type ListSSHKeyPairs struct {
-	Account     string `json:"account,omitempty"`
-	DomainID    string `json:"domainid,omitempty"`
-	Fingerprint string `json:"fingerprint,omitempty"`
-	IsRecursive *bool  `json:"isrecursive,omitempty"`
-	Keyword     string `json:"keyword,omitempty"`
-	ListAll     *bool  `json:"listall,omitempty"`
-	Name        string `json:"name,omitempty"`
+	Account     string `json:"account,omitempty" doc:"list resources by account. Must be used with the domainId parameter."`
+	DomainID    string `json:"domainid,omitempty" doc:"list only resources belonging to the domain specified"`
+	Fingerprint string `json:"fingerprint,omitempty" doc:"A public key fingerprint to look for"`
+	IsRecursive *bool  `json:"isrecursive,omitempty" doc:"defaults to false, but if true, lists all resources from the parent specified by the domainId till leaves."`
+	Keyword     string `json:"keyword,omitempty" doc:"List by keyword"`
+	ListAll     *bool  `json:"listall,omitempty" doc:"If set to false, list only resources belonging to the command's caller; if set to true - list resources that the caller is authorized to see. Default value is false"`
+	Name        string `json:"name,omitempty" doc:"A key pair name to look for"`
 	Page        int    `json:"page,omitempty"`
 	PageSize    int    `json:"pagesize,omitempty"`
-	ProjectID   string `json:"projectid,omitempty"`
+	ProjectID   string `json:"projectid,omitempty" doc:"list objects by project"`
 }
 
 // ListSSHKeyPairsResponse represents a list of SSH key pairs
@@ -79,11 +79,11 @@ type ListSSHKeyPairsResponse struct {
 //
 // CloudStack API: http://cloudstack.apache.org/api/apidocs-4.10/apis/resetSSHKeyForVirtualMachine.html
 type ResetSSHKeyForVirtualMachine struct {
-	ID        string `json:"id"`
-	KeyPair   string `json:"keypair"`
-	Account   string `json:"account,omitempty"`
-	DomainID  string `json:"domainid,omitempty"`
-	ProjectID string `json:"projectid,omitempty"`
+	ID        string `json:"id" doc:"The ID of the virtual machine"`
+	KeyPair   string `json:"keypair" doc:"name of the ssh key pair used to login to the virtual machine"`
+	Account   string `json:"account,omitempty" doc:"an optional account for the ssh key. Must be used with domainId."`
+	DomainID  string `json:"domainid,omitempty" doc:"an optional domainId for the virtual machine. If the account parameter is used, domainId must also be used."`
+	ProjectID string `json:"projectid,omitempty" doc:"an optional project for the ssh key"`
 }
 
 // ResetSSHKeyForVirtualMachineResponse represents the modified VirtualMachine
