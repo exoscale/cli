0cac59cc61a3cd19a9e134a210dc256b0776a5c4
Yoan Blanc <yoan.blanc@exoscale.ch>
2018-05-01 13:38:27 +0200

    vm groups: add documentation
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

--- vm_groups.go
+++ vm_groups.go
@@ -2,14 +2,14 @@ package egoscale
 
 // InstanceGroup represents a group of VM
 type InstanceGroup struct {
-	ID        string `json:"id"`
-	Account   string `json:"account,omitempty"`
-	Created   string `json:"created,omitempty"`
-	Domain    string `json:"domain,omitempty"`
-	DomainID  string `json:"domainid,omitempty"`
-	Name      string `json:"name,omitempty"`
-	Project   string `json:"project,omitempty"`
-	ProjectID string `json:"projectid,omitempty"`
+	Account   string `json:"account,omitempty" doc:"the account owning the instance group"`
+	Created   string `json:"created,omitempty" doc:"time and date the instance group was created"`
+	Domain    string `json:"domain,omitempty" doc:"the domain name of the instance group"`
+	DomainID  string `json:"domainid,omitempty" doc:"the domain ID of the instance group"`
+	ID        string `json:"id,omitempty" doc:"the id of the instance group"`
+	Name      string `json:"name,omitempty" doc:"the name of the instance group"`
+	Project   string `json:"project,omitempty" doc:"the project name of the group"`
+	ProjectID string `json:"projectid,omitempty" doc:"the project id of the group"`
 }
 
 // InstanceGroupResponse represents a VM group
@@ -21,10 +21,10 @@ type InstanceGroupResponse struct {
 //
 // CloudStack API: http://cloudstack.apache.org/api/apidocs-4.10/apis/createInstanceGroup.html
 type CreateInstanceGroup struct {
-	Name      string `json:"name"`
-	Account   string `json:"account,omitempty"`
-	DomainID  string `json:"domainid,omitempty"`
-	ProjectID string `json:"projectid,omitempty"`
+	Name      string `json:"name" doc:"the name of the instance group"`
+	Account   string `json:"account,omitempty" doc:"the account of the instance group. The account parameter must be used with the domainId parameter."`
+	DomainID  string `json:"domainid,omitempty" doc:"the domain ID of account owning the instance group"`
+	ProjectID string `json:"projectid,omitempty" doc:"The project of the instance group"`
 }
 
 // name returns the CloudStack API command name
@@ -39,12 +39,12 @@ func (*CreateInstanceGroup) response() interface{} {
 // CreateInstanceGroupResponse represents a freshly created VM group
 type CreateInstanceGroupResponse InstanceGroupResponse
 
-// UpdateInstanceGroup creates a VM group
+// UpdateInstanceGroup updates a VM group
 //
 // CloudStack API: http://cloudstack.apache.org/api/apidocs-4.10/apis/updateInstanceGroup.html
 type UpdateInstanceGroup struct {
-	ID   string `json:"id"`
-	Name string `json:"name,omitempty"`
+	ID   string `json:"id" doc:"Instance group ID"`
+	Name string `json:"name,omitempty" doc:"new instance group name"`
 }
 
 // name returns the CloudStack API command name
@@ -59,14 +59,11 @@ func (*UpdateInstanceGroup) response() interface{} {
 // UpdateInstanceGroupResponse represents an updated VM group
 type UpdateInstanceGroupResponse InstanceGroupResponse
 
-// DeleteInstanceGroup creates a VM group
+// DeleteInstanceGroup deletes a VM group
 //
 // CloudStack API: http://cloudstack.apache.org/api/apidocs-4.10/apis/deleteInstanceGroup.html
 type DeleteInstanceGroup struct {
-	Name      string `json:"name"`
-	Account   string `json:"account,omitempty"`
-	DomainID  string `json:"domainid,omitempty"`
-	ProjectID string `json:"projectid,omitempty"`
+	ID string `json:"id" doc:"the ID of the instance group"`
 }
 
 // name returns the CloudStack API command name
@@ -82,16 +79,16 @@ func (*DeleteInstanceGroup) response() interface{} {
 //
 // CloudStack API: http://cloudstack.apache.org/api/apidocs-4.10/apis/listInstanceGroups.html
 type ListInstanceGroups struct {
-	Account     string `json:"account,omitempty"`
-	DomainID    string `json:"domainid,omitempty"`
-	ID          string `json:"id,omitempty"`
-	IsRecursive *bool  `json:"isrecursive,omitempty"`
-	Keyword     string `json:"keyword,omitempty"`
-	ListAll     *bool  `json:"listall,omitempty"`
+	Account     string `json:"account,omitempty" doc:"list resources by account. Must be used with the domainId parameter."`
+	DomainID    string `json:"domainid,omitempty" doc:"list only resources belonging to the domain specified"`
+	ID          string `json:"id,omitempty" doc:"list instance groups by ID"`
+	IsRecursive *bool  `json:"isrecursive,omitempty" doc:"defaults to false, but if true, lists all resources from the parent specified by the domainId till leaves."`
+	Keyword     string `json:"keyword,omitempty" doc:"List by keyword"`
+	ListAll     *bool  `json:"listall,omitempty" doc:"If set to false, list only resources belonging to the command's caller; if set to true - list resources that the caller is authorized to see. Default value is false"`
+	Name        string `json:"name,omitempty" doc:"list instance groups by name"`
 	Page        int    `json:"page,omitempty"`
 	PageSize    int    `json:"pagesize,omitempty"`
-	State       string `json:"state,omitempty"`
-	ProjectID   string `json:"projectid,omitempty"`
+	ProjectID   string `json:"projectid,omitempty" doc:"list objects by project"`
 }
 
 // name returns the CloudStack API command name
