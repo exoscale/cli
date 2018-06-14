dceb196136c3eb329ddd76fa1b1ee511897845ed
Yoan Blanc <yoan.blanc@exoscale.ch>
2018-06-14 09:01:19 +0200

    website: extract the setup (for @ste-m)
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

---
+++ website/prepare.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -xe
+
+cd ../
+dep ensure -vendor-only
+
+cd cmd/cs
+dep ensure -vendor-only
+go build
+./cs gen-doc
+cp README.md ../../website/content/cs/_index.md
+
+cd ../exo
+dep ensure -vendor-only
+go run doc/main.go
+
+set +xe
+echo "we are now ready to run hugo"
