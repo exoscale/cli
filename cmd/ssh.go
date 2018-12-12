ae5341f52b2caa01d338ad30fcad2935d166b5e7
Yoan Blanc <yoan.blanc@exoscale.ch>
2018-12-12 07:36:10 +0100

    ssh: name can be ambiguous...
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

--- cmd/ssh.go
+++ cmd/ssh.go
@@ -63,9 +63,46 @@ type sshInfo struct {
 }
 
 func getSSHInfo(name string, isIpv6 bool) (*sshInfo, error) {
-	vm, err := getVMWithNameOrID(name)
+	vmQuery := egoscale.VirtualMachine{}
+	id, err := egoscale.ParseUUID(name)
+	if err != nil {
+		vmQuery.Name = name
+	} else {
+		vmQuery.ID = id
+	}
+
+	vms, err := cs.ListWithContext(gContext, vmQuery)
 	if err != nil {
 		return nil, err
+	}
+
+	var vm *egoscale.VirtualMachine
+	switch len(vms) {
+	case 0:
+		return nil, fmt.Errorf("no VMs has been found")
+	case 1:
+		vm = vms[0].(*egoscale.VirtualMachine)
+	default:
+		names := []string{}
+		for _, i := range vms {
+			v := i.(*egoscale.VirtualMachine)
+			if v.Name != vmQuery.Name {
+				continue
+			}
+
+			vm = v
+			names = append(names, fmt.Sprintf("\t%s\t%s\t%s", v.ID.String(), v.ZoneName, v.IP()))
+		}
+
+		if len(names) == 1 {
+			break
+		}
+
+		fmt.Println("More than one VM has been found, use the ID instead:")
+		for _, name := range names {
+			fmt.Println(name)
+		}
+		return nil, fmt.Errorf("abort vm name %q is ambiguous", vmQuery.Name)
 	}
 
 	sshKeyPath := path.Join(gConfigFolder, "instances", vm.ID.String(), "id_rsa")
