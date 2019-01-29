1544298d606209904a03a9dde59a259d65fe8e05
pierre-emmanuelJ <pierre-emmanuel.jacquier@epitech.eu>
2019-01-29 15:27:05 +0100

    exo sshkey delete --all (#87)
    Signed-off-by: Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>
    Reviewed-by: Mathieu Corbin <corbin.math@gmail.com>

--- cmd/sshkey_delete.go
+++ cmd/sshkey_delete.go
@@ -9,12 +9,29 @@ import (
 
 // deleteCmd represents the delete command
 var sshkeyDeleteCmd = &cobra.Command{
-	Use:     "delete <name>",
+	Use:     "delete [name]+",
 	Short:   "Delete SSH key pair",
 	Aliases: gDeleteAlias,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) < 1 {
+		all, err := cmd.Flags().GetBool("all")
+		if err != nil {
+			return err
+		}
+
+		if len(args) < 1 && !all {
 			return cmd.Usage()
+		}
+
+		sshKeys := []egoscale.SSHKeyPair{}
+		if all {
+			sshKeys, err = getSSHKeys(cs)
+			if err != nil {
+				return err
+			}
+		} else {
+			for _, k := range args {
+				sshKeys = append(sshKeys, egoscale.SSHKeyPair{Name: k})
+			}
 		}
 
 		force, err := cmd.Flags().GetBool("force")
@@ -22,32 +39,32 @@ var sshkeyDeleteCmd = &cobra.Command{
 			return err
 		}
 
-		if !force {
-			if !askQuestion(fmt.Sprintf("Are you sure you want to delete %q SSH key pair", args[0])) {
-				return nil
+		//XXX Create a function to execute non async tasks asynchronousely
+		for _, sshkey := range sshKeys {
+
+			if !force {
+				if !askQuestion(fmt.Sprintf("Are you sure you want to delete %q SSH key pair", sshkey.Name)) {
+					continue
+				}
 			}
+
+			if err := deleteSSHKey(sshkey.Name); err != nil {
+				return err
+			}
+			fmt.Println(sshkey.Name)
 		}
 
-		res, err := deleteSSHKey(args[0])
-		if err != nil {
-			return err
-		}
-
-		fmt.Println(res)
 		return nil
 	},
 }
 
-func deleteSSHKey(name string) (string, error) {
+func deleteSSHKey(name string) error {
 	sshKey := &egoscale.DeleteSSHKeyPair{Name: name}
-	if err := cs.BooleanRequestWithContext(gContext, sshKey); err != nil {
-		return "", err
-	}
-
-	return sshKey.Name, nil
+	return cs.BooleanRequestWithContext(gContext, sshKey)
 }
 
 func init() {
 	sshkeyDeleteCmd.Flags().BoolP("force", "f", false, "Attempt to remove SSH key pair without prompting for confirmation")
+	sshkeyDeleteCmd.Flags().BoolP("all", "", false, "Remove all SSH keys")
 	sshkeyCmd.AddCommand(sshkeyDeleteCmd)
 }
