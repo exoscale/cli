cabd7efcc5fbd1426b7c1f6f4e3353f8deb54d26
Marc Falzon <marc@exoscale.com>
2019-05-13 17:00:26 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Normalize "show" output format across commands

--- cmd/runstatus_maintenance_show.go
+++ cmd/runstatus_maintenance_show.go
@@ -3,57 +3,67 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"text/tabwriter"
+	"strings"
 
+	"github.com/exoscale/cli/table"
 	"github.com/exoscale/egoscale"
 	"github.com/spf13/cobra"
 )
 
-// runstatusMaintenanceShowCmd represents the show command
-var runstatusMaintenanceShowCmd = &cobra.Command{
-	Use:     "show [page name] <maintenance name>",
-	Short:   "Show a maintenance",
-	Aliases: gShowAlias,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) < 1 {
-			return cmd.Usage()
-		}
+func init() {
+	runstatusMaintenanceCmd.AddCommand(
+		&cobra.Command{
+			Use:     "show [page name] <maintenance name|id>",
+			Short:   "Show a runstat.us page maintenance details",
+			Aliases: gShowAlias,
+			RunE: func(cmd *cobra.Command, args []string) error {
+				if len(args) < 1 {
+					return cmd.Usage()
+				}
 
-		pageName := gCurrentAccount.DefaultRunstatusPage
-		serviceName := args[0]
+				page := gCurrentAccount.DefaultRunstatusPage
+				maintenance := args[0]
 
-		if gCurrentAccount.DefaultRunstatusPage == "" && len(args) == 1 {
-			fmt.Fprintf(os.Stderr, `Error: No default runstat.us page is set:
+				if gCurrentAccount.DefaultRunstatusPage == "" && len(args) == 1 {
+					fmt.Fprintf(os.Stderr, `Error: No default runstat.us page is set:
   Please specify a page in parameter or add it to %q
 
   `, gConfigFilePath)
-			return cmd.Usage()
-		}
+					return cmd.Usage()
+				}
 
-		if len(args) > 1 {
-			pageName = args[0]
-			serviceName = args[1]
-		}
+				if len(args) > 1 {
+					page = args[0]
+					maintenance = args[1]
+				}
 
-		runstatusPage, err := csRunstatus.GetRunstatusPage(gContext, egoscale.RunstatusPage{Subdomain: pageName})
-		if err != nil {
-			return err
-		}
-
-		maintenance, err := getMaintenanceByNameOrID(*runstatusPage, serviceName)
-		if err != nil {
-			return err
-		}
-
-		w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.TabIndent)
-		fmt.Fprintf(w, "Title:\t%s\n", maintenance.Title)  // nolint: errcheck
-		fmt.Fprintf(w, "State:\t%s\n", maintenance.Status) // nolint: errcheck
-		fmt.Fprintf(w, "URL:\t%s\n", maintenance.URL)      // nolint: errcheck
-
-		return w.Flush()
-	},
+				return showRunstatusMaintenance(page, maintenance)
+			},
+		})
 }
 
-func init() {
-	runstatusMaintenanceCmd.AddCommand(runstatusMaintenanceShowCmd)
+func showRunstatusMaintenance(p, m string) error {
+	page, err := csRunstatus.GetRunstatusPage(gContext, egoscale.RunstatusPage{Subdomain: p})
+	if err != nil {
+		return err
+	}
+
+	maintenance, err := getMaintenanceByNameOrID(*page, m)
+	if err != nil {
+		return err
+	}
+	t := table.NewTable(os.Stdout)
+	t.SetHeader([]string{page.Subdomain})
+
+	t.Append([]string{"ID", fmt.Sprint(maintenance.ID)})
+	t.Append([]string{"Title", maintenance.Title})
+	t.Append([]string{"Description", maintenance.Description})
+	t.Append([]string{"State", maintenance.Status})
+	t.Append([]string{"Start Date", fmt.Sprint(maintenance.StartDate)})
+	t.Append([]string{"End Date", fmt.Sprint(maintenance.EndDate)})
+	t.Append([]string{"Affected Services", strings.Join(maintenance.Services, "\n")})
+
+	t.Render()
+
+	return nil
 }
