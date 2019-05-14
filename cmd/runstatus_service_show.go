a0e3fda073172beee4da8278e88005817be0953f
Marc Falzon <marc@exoscale.com>
2019-05-14 10:38:08 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Normalize "show" output format across commands

--- cmd/runstatus_service_show.go
+++ cmd/runstatus_service_show.go
@@ -3,57 +3,62 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"text/tabwriter"
 
+	"github.com/exoscale/cli/table"
 	"github.com/exoscale/egoscale"
 	"github.com/spf13/cobra"
 )
 
-// runstatusServiceShowCmd represents the show command
-var runstatusServiceShowCmd = &cobra.Command{
-	Use:     "show [page name] <service name>",
-	Short:   "Show a service",
-	Aliases: gShowAlias,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) < 1 {
-			return cmd.Usage()
-		}
+func init() {
+	runstatusServiceCmd.AddCommand(&cobra.Command{
+		Use:     "show [page name] <service name>",
+		Short:   "Show a service",
+		Aliases: gShowAlias,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return cmd.Usage()
+			}
 
-		pageName := gCurrentAccount.DefaultRunstatusPage
-		serviceName := args[0]
+			page := gCurrentAccount.DefaultRunstatusPage
+			service := args[0]
 
-		if gCurrentAccount.DefaultRunstatusPage == "" && len(args) == 1 {
-			fmt.Fprintf(os.Stderr, `Error: No default runstat.us page is set:
+			if gCurrentAccount.DefaultRunstatusPage == "" && len(args) == 1 {
+				fmt.Fprintf(os.Stderr, `Error: No default runstat.us page is set:
   Please specify a page in parameter or add it to %q
 
   `, gConfigFilePath)
-			return cmd.Usage()
-		}
+				return cmd.Usage()
+			}
 
-		if len(args) > 1 {
-			pageName = args[0]
-			serviceName = args[1]
-		}
+			if len(args) > 1 {
+				page = args[0]
+				service = args[1]
+			}
 
-		runstatusPage, err := csRunstatus.GetRunstatusPage(gContext, egoscale.RunstatusPage{Subdomain: pageName})
-		if err != nil {
-			return err
-		}
-
-		service, err := getServiceByName(*runstatusPage, serviceName)
-		if err != nil {
-			return err
-		}
-
-		w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.TabIndent)
-		fmt.Fprintf(w, "Name:\t%s\n", service.Name)   // nolint: errcheck
-		fmt.Fprintf(w, "State:\t%s\n", service.State) // nolint: errcheck
-		fmt.Fprintf(w, "URL:\t%s\n", service.URL)     // nolint: errcheck
-
-		return w.Flush()
-	},
+			return showRunstatusService(page, service)
+		},
+	})
 }
 
-func init() {
-	runstatusServiceCmd.AddCommand(runstatusServiceShowCmd)
+func showRunstatusService(p, s string) error {
+	page, err := csRunstatus.GetRunstatusPage(gContext, egoscale.RunstatusPage{Subdomain: p})
+	if err != nil {
+		return err
+	}
+
+	service, err := getServiceByName(*page, s)
+	if err != nil {
+		return err
+	}
+
+	t := table.NewTable(os.Stdout)
+	t.SetHeader([]string{page.Subdomain})
+
+	t.Append([]string{"ID", fmt.Sprint(service.ID)})
+	t.Append([]string{"Name", service.Name})
+	t.Append([]string{"State", service.State})
+
+	t.Render()
+
+	return nil
 }
