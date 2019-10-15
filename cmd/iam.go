7f0768e0c52c0f342eaedd7b85578f797cc225ca
Johnatan Illi <johnatan.illi@exoscale.ch>
2019-10-15 12:01:34 +0200

    fixup! fix review & update vendor

--- cmd/iam.go
+++ cmd/iam.go
@@ -2,7 +2,7 @@ package cmd
 
 import "github.com/spf13/cobra"
 
-// iamCmd represent the API key command
+// iamCmd represent the IAM command
 var iamCmd = &cobra.Command{
 	Use:   "iam",
 	Short: "Identity and Access Management cmd",
