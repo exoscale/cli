f4eadc822cb1bde7dfedb7b3114361025f765351
illi-j <16138690+illi-j@users.noreply.github.com>
2019-11-15 12:13:40 +0100

    add list API key Operations (#199)

---
+++ cmd/iam_apikey_operations.go
@@ -0,0 +1,76 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/exoscale/egoscale"
+	"github.com/spf13/cobra"
+)
+
+type apiKeyOperationsItemOutput struct {
+	Compute []string `json:"compute,omitempty"`
+	DNS     []string `json:"dns,omitempty"`
+	IAM     []string `json:"iam,omitempty"`
+	SOS     []string `json:"sos,omitempty"`
+}
+
+func (o *apiKeyOperationsItemOutput) toJSON()  { outputJSON(o) }
+func (o *apiKeyOperationsItemOutput) toText()  { outputText(o) }
+func (o *apiKeyOperationsItemOutput) toTable() { outputTable(o) }
+
+// apiKeyOperationsCmd represents the supported operations listing command for an API key
+var apiKeyOperationsCmd = &cobra.Command{
+	Use:   "operations [filter ...]",
+	Short: "List supported API key operations",
+	Long: fmt.Sprintf(`This command lists all supported operations for an API key.
+	Optional patterns can be provided to filter results by compute, DNS, IAM or SOS operations.
+
+	Supported output template annotations: %s`,
+		strings.Join(outputterTemplateAnnotations(&apiKeyOperationsItemOutput{}), ", ")),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return output(listAPIKeyOperations(args))
+	},
+}
+
+func listAPIKeyOperations(filters []string) (outputter, error) {
+	resp, err := cs.RequestWithContext(gContext, &egoscale.ListAPIKeyOperations{})
+	if err != nil {
+		return nil, err
+	}
+
+	opes := resp.(*egoscale.ListAPIKeyOperationsResponse)
+
+	out := apiKeyOperationsItemOutput{}
+
+	for _, o := range opes.Operations {
+		operation := strings.ToLower(o)
+
+		result := operation
+		for _, f := range filters {
+			result = ""
+			filter := strings.ToLower(f)
+			if strings.Contains(operation, filter) {
+				result = operation
+				break
+			}
+		}
+
+		switch true {
+		case strings.Contains(result, "compute"):
+			out.Compute = append(out.Compute, result)
+		case strings.Contains(result, "dns"):
+			out.DNS = append(out.DNS, result)
+		case strings.Contains(result, "iam"):
+			out.IAM = append(out.IAM, result)
+		case strings.Contains(result, "sos"):
+			out.SOS = append(out.SOS, result)
+		}
+	}
+
+	return &out, nil
+}
+
+func init() {
+	apiKeyCmd.AddCommand(apiKeyOperationsCmd)
+}
