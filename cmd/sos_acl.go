f4e6cdcc51543fbabf0e7e04ce16d496be61acc3
Chris Glass <chris@exoscale.ch>
2019-12-09 16:48:07 +0100

    Print SOS link when adding canned public ACL (#208)
    Print SOS link when adding canned public ACL
    
    In an overwhelming majority of cases, users setting ACLs to
    --public-read or --public-read-write will want to share the link to that
    ressource via HTTP.
    
    This saves the users a couple of clicks by printing the link to the file
    they just uploaded to stdout.
    
    No other canned ACLs are impacted.

--- cmd/sos_acl.go
+++ cmd/sos_acl.go
@@ -115,7 +115,23 @@ var sosAddACLCmd = &cobra.Command{
 		}
 
 		// Copy object call
-		return sosClient.CopyObject(dst, src)
+		err = sosClient.CopyObject(dst, src)
+		if err != nil {
+			return err
+		}
+
+		if !gQuiet {
+			acl, err := getDefaultCannedACL(cmd)
+			if err != nil {
+				return err
+			}
+
+			if acl == publicReadWrite || acl == publicRead {
+				fmt.Printf("https://sos-%s.exo.io/%s/%s\n", location, bucket, object)
+			}
+		}
+
+		return nil
 	},
 }
 
