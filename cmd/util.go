f97d1b6b27482f91b48b3d5a426d881614a42f2e
Marc Falzon <marc@exoscale.com>
2021-06-18 11:54:25 +0200

    lab: db: fix crash during types list/show
    This change fixes as crashing bug occuring during `exo lab db types
    list|show` commands.

--- cmd/util.go
+++ cmd/util.go
@@ -22,3 +22,12 @@ func ellipString(s string, maxLen int) string {
 
 	return s
 }
+
+// defaultString returns the value of the string pointer s if not nil, otherwise the default value specified.
+func defaultString(s *string, def string) string {
+	if s != nil {
+		return *s
+	}
+
+	return def
+}
