a5fd9e20d59f25c93044e04f46cb16596f7151f8
Marc Falzon <marc@exoscale.com>
2021-08-10 16:41:11 +0200

    compute: instance: add "snapshot" commands
    This change adds new `exo compute instance snapshot *` commands.

---
+++ cmd/instance_snapshot_create.go
@@ -0,0 +1,73 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	egoscale "github.com/exoscale/egoscale/v2"
+	exoapi "github.com/exoscale/egoscale/v2/api"
+	"github.com/spf13/cobra"
+)
+
+type instanceSnapshotCreateCmd struct {
+	cliCommandSettings `cli-cmd:"-"`
+
+	_ bool `cli-cmd:"create"`
+
+	Instance string `cli-arg:"#" cli-usage:"INSTANCE-NAME|ID"`
+
+	Zone string `cli-short:"z" cli-usage:"instance zone"`
+}
+
+func (c *instanceSnapshotCreateCmd) cmdAliases() []string { return gCreateAlias }
+
+func (c *instanceSnapshotCreateCmd) cmdShort() string { return "Create a Compute instance snapshot" }
+
+func (c *instanceSnapshotCreateCmd) cmdLong() string {
+	return fmt.Sprintf(`This command creates a Compute instance snapshot.
+
+Supported output template annotations: %s`,
+		strings.Join(outputterTemplateAnnotations(&instanceShowOutput{}), ", "))
+}
+
+func (c *instanceSnapshotCreateCmd) cmdPreRun(cmd *cobra.Command, args []string) error {
+	cmdSetZoneFlagFromDefault(cmd)
+	return cliCommandDefaultPreRun(c, cmd, args)
+}
+
+func (c *instanceSnapshotCreateCmd) cmdRun(_ *cobra.Command, _ []string) error {
+	// Snapshot creation can take a _long time_, raising
+	// the Exoscale API client timeout as a precaution.
+	cs.Client.SetTimeout(30 * time.Minute)
+
+	ctx := exoapi.WithEndpoint(gContext, exoapi.NewReqEndpoint(gCurrentAccount.Environment, c.Zone))
+
+	instance, err := cs.FindInstance(ctx, c.Zone, c.Instance)
+	if err != nil {
+		return err
+	}
+
+	var snapshot *egoscale.Snapshot
+	decorateAsyncOperation(fmt.Sprintf("Creating snapshot of instance %q...", c.Instance), func() {
+		snapshot, err = instance.CreateSnapshot(ctx)
+		if err != nil {
+			return
+		}
+	})
+	if err != nil {
+		return err
+	}
+
+	if !gQuiet {
+		return output(showInstanceSnapshot(c.Zone, *snapshot.ID))
+	}
+
+	return nil
+}
+
+func init() {
+	cobra.CheckErr(registerCLICommand(computeInstanceSnapshotCmd, &instanceSnapshotCreateCmd{
+		cliCommandSettings: defaultCLICmdSettings(),
+	}))
+}
---
+++ cmd/instance_snapshot_show.go
@@ -0,0 +1,83 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	exoapi "github.com/exoscale/egoscale/v2/api"
+	"github.com/spf13/cobra"
+)
+
+type instanceSnapshotShowOutput struct {
+	ID           string `json:"id"`
+	Name         string `json:"name"`
+	CreationDate string `json:"creation_date"`
+	State        string `json:"state"`
+	Instance     string `json:"instance"`
+	Zone         string `json:"zone"`
+}
+
+func (o *instanceSnapshotShowOutput) toJSON()  { outputJSON(o) }
+func (o *instanceSnapshotShowOutput) toText()  { outputText(o) }
+func (o *instanceSnapshotShowOutput) toTable() { outputTable(o) }
+
+type instanceSnapshotShowCmd struct {
+	cliCommandSettings `cli-cmd:"-"`
+
+	_ bool `cli-cmd:"show"`
+
+	ID string `cli-arg:"#"`
+
+	Zone string `cli-short:"z" cli-usage:"snapshot zone"`
+}
+
+func (c *instanceSnapshotShowCmd) cmdAliases() []string { return gShowAlias }
+
+func (c *instanceSnapshotShowCmd) cmdShort() string {
+	return "Show a Compute instance snapshot details"
+}
+
+func (c *instanceSnapshotShowCmd) cmdLong() string {
+	return fmt.Sprintf(`This command shows a Compute instance snapshot details.
+
+	Supported output template annotations: %s`,
+		strings.Join(outputterTemplateAnnotations(&instanceSnapshotShowOutput{}), ", "))
+}
+
+func (c *instanceSnapshotShowCmd) cmdPreRun(cmd *cobra.Command, args []string) error {
+	cmdSetZoneFlagFromDefault(cmd)
+	return cliCommandDefaultPreRun(c, cmd, args)
+}
+
+func (c *instanceSnapshotShowCmd) cmdRun(_ *cobra.Command, _ []string) error {
+	return output(showInstanceSnapshot(c.Zone, c.ID))
+}
+
+func showInstanceSnapshot(zone, snapshotID string) (outputter, error) {
+	ctx := exoapi.WithEndpoint(gContext, exoapi.NewReqEndpoint(gCurrentAccount.Environment, zone))
+
+	snapshot, err := cs.GetSnapshot(ctx, zone, snapshotID)
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving Compute instance snapshot: %s", err)
+	}
+
+	instance, err := cs.GetInstance(ctx, zone, *snapshot.InstanceID)
+	if err != nil {
+		return nil, fmt.Errorf("unable to retrieve Compute instance %q: %s", *snapshot.InstanceID, err)
+	}
+
+	return &instanceSnapshotShowOutput{
+		ID:           *snapshot.ID,
+		Name:         *snapshot.Name,
+		CreationDate: snapshot.CreatedAt.String(),
+		State:        *snapshot.State,
+		Instance:     *instance.Name,
+		Zone:         zone,
+	}, nil
+}
+
+func init() {
+	cobra.CheckErr(registerCLICommand(computeInstanceSnapshotCmd, &instanceSnapshotShowCmd{
+		cliCommandSettings: defaultCLICmdSettings(),
+	}))
+}
