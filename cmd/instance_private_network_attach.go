0ecd2e5eb0f3fd91a467fd9116c2792b42d42306
Marc Falzon <marc@exoscale.com>
2021-09-06 17:14:08 +0200

    compute: instance: overhaul "privnet" commands
    This change renames the `exo compute instance private-network
    (add|remove)` commands to `exo compute instance private-network
    attach|detach)`, which now only accept only a single Private Network per
    execution rather than a list.

---
+++ cmd/instance_private_network_attach.go
@@ -0,0 +1,86 @@
+package cmd
+
+import (
+	"fmt"
+	"net"
+	"strings"
+
+	exoapi "github.com/exoscale/egoscale/v2/api"
+	"github.com/spf13/cobra"
+)
+
+type instancePrivnetAttachCmd struct {
+	cliCommandSettings `cli-cmd:"-"`
+
+	_ bool `cli-cmd:"attach"`
+
+	Instance       string `cli-arg:"#" cli-usage:"INSTANCE-NAME|ID"`
+	PrivateNetwork string `cli-arg:"#" cli-usage:"PRIVATE-NETWORK-NAME|ID"`
+
+	IPAddress string `cli-flag:"ip" cli-usage:"network IP address to assign to the Compute instance (managed Private Networks only)"`
+	Zone      string `cli-short:"z" cli-usage:"instance zone"`
+}
+
+func (c *instancePrivnetAttachCmd) cmdAliases() []string { return nil }
+
+func (c *instancePrivnetAttachCmd) cmdShort() string {
+	return "Attach a Compute instance to a Private Network"
+}
+
+func (c *instancePrivnetAttachCmd) cmdLong() string {
+	return fmt.Sprintf(`This command attaches a Compute instance to a Private Network.
+
+Supported output template annotations: %s`,
+		strings.Join(outputterTemplateAnnotations(&instanceShowOutput{}), ", "),
+	)
+}
+
+func (c *instancePrivnetAttachCmd) cmdPreRun(cmd *cobra.Command, args []string) error {
+	cmdSetZoneFlagFromDefault(cmd)
+	return cliCommandDefaultPreRun(c, cmd, args)
+}
+
+func (c *instancePrivnetAttachCmd) cmdRun(_ *cobra.Command, _ []string) error {
+	ctx := exoapi.WithEndpoint(gContext, exoapi.NewReqEndpoint(gCurrentAccount.Environment, c.Zone))
+
+	instance, err := cs.FindInstance(ctx, c.Zone, c.Instance)
+	if err != nil {
+		return err
+	}
+
+	privateNetwork, err := cs.FindPrivateNetwork(ctx, c.Zone, c.PrivateNetwork)
+	if err != nil {
+		return fmt.Errorf("error retrieving Private Network: %s", err)
+	}
+
+	decorateAsyncOperation(fmt.Sprintf(
+		"Attaching instance %q to Private Network %q...",
+		c.Instance,
+		c.PrivateNetwork,
+	), func() {
+		if err = cs.AttachInstanceToPrivateNetwork(
+			ctx,
+			c.Zone,
+			instance,
+			privateNetwork,
+			net.ParseIP(c.IPAddress),
+		); err != nil {
+			return
+		}
+	})
+	if err != nil {
+		return err
+	}
+
+	if !gQuiet {
+		return output(showInstance(c.Zone, *instance.ID))
+	}
+
+	return nil
+}
+
+func init() {
+	cobra.CheckErr(registerCLICommand(computeInstancePrivnetCmd, &instancePrivnetAttachCmd{
+		cliCommandSettings: defaultCLICmdSettings(),
+	}))
+}
---
+++ cmd/instance_private_network_detach.go
@@ -0,0 +1,78 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	exoapi "github.com/exoscale/egoscale/v2/api"
+	"github.com/spf13/cobra"
+)
+
+type instancePrivnetDetachCmd struct {
+	cliCommandSettings `cli-cmd:"-"`
+
+	_ bool `cli-cmd:"detach"`
+
+	Instance       string `cli-arg:"#" cli-usage:"INSTANCE-NAME|ID"`
+	PrivateNetwork string `cli-arg:"#" cli-usage:"PRIVATE-NETWORK-NAME|ID"`
+
+	Zone string `cli-short:"z" cli-usage:"instance zone"`
+}
+
+func (c *instancePrivnetDetachCmd) cmdAliases() []string { return nil }
+
+func (c *instancePrivnetDetachCmd) cmdShort() string {
+	return "Detach a Compute instance from a Private Network"
+}
+
+func (c *instancePrivnetDetachCmd) cmdLong() string {
+	return fmt.Sprintf(`This command detaches a Compute instance from a Private Network.
+
+Supported output template annotations: %s`,
+		strings.Join(outputterTemplateAnnotations(&instanceShowOutput{}), ", "),
+	)
+}
+
+func (c *instancePrivnetDetachCmd) cmdPreRun(cmd *cobra.Command, args []string) error {
+	cmdSetZoneFlagFromDefault(cmd)
+	return cliCommandDefaultPreRun(c, cmd, args)
+}
+
+func (c *instancePrivnetDetachCmd) cmdRun(_ *cobra.Command, _ []string) error {
+	ctx := exoapi.WithEndpoint(gContext, exoapi.NewReqEndpoint(gCurrentAccount.Environment, c.Zone))
+
+	instance, err := cs.FindInstance(ctx, c.Zone, c.Instance)
+	if err != nil {
+		return err
+	}
+
+	privateNetwork, err := cs.FindPrivateNetwork(ctx, c.Zone, c.PrivateNetwork)
+	if err != nil {
+		return fmt.Errorf("error retrieving Private Network: %s", err)
+	}
+
+	decorateAsyncOperation(fmt.Sprintf(
+		"Detaching instance %q from Private Network %q...",
+		c.Instance,
+		c.PrivateNetwork,
+	), func() {
+		if err = cs.DetachInstanceFromPrivateNetwork(ctx, c.Zone, instance, privateNetwork); err != nil {
+			return
+		}
+	})
+	if err != nil {
+		return err
+	}
+
+	if !gQuiet {
+		return output(showInstance(c.Zone, *instance.ID))
+	}
+
+	return nil
+}
+
+func init() {
+	cobra.CheckErr(registerCLICommand(computeInstancePrivnetCmd, &instancePrivnetDetachCmd{
+		cliCommandSettings: defaultCLICmdSettings(),
+	}))
+}
