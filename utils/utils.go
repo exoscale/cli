9be3100adb722162a840512a7ab0030eaa8a1f58
Marc Falzon <marc@exoscale.com>
2021-09-08 18:04:23 +0200

    Remove dead code

--- utils/utils.go
+++ utils/utils.go
@@ -1,12 +1,11 @@
 package utils
 
 import (
-	"bytes"
 	"crypto/rand"
 	"encoding/base64"
 )
 
-//RandStringBytes Generate random string of n bytes
+// RandStringBytes Generate random string of n bytes
 func RandStringBytes(n int) (string, error) {
 	b := make([]byte, n)
 	_, err := rand.Read(b)
@@ -14,15 +13,4 @@ func RandStringBytes(n int) (string, error) {
 		return "", err
 	}
 	return base64.StdEncoding.EncodeToString(b), nil
-}
-
-//FastStringConcat concat string faster
-func FastStringConcat(strings ...string) string {
-	var buffer bytes.Buffer
-
-	for i := 0; i < len(strings); i++ {
-		buffer.WriteString(strings[i])
-	}
-
-	return buffer.String()
 }
