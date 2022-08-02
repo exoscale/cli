e5028fdecbcd682666604d0431d5ec6a8b6d9fef
Predrag Janosevic <predrag.janosevic@exoscale.com>
2022-08-02 21:15:12 +0200

    dbaas_show_opensearch: fix nil pointer dereference error

--- cmd/util.go
+++ cmd/util.go
@@ -57,6 +57,15 @@ func defaultIp(i *net.IP, def string) string {
 	return def
 }
 
+// defaultInt64 returns the value of the int64 pointer b if not nil, otherwise the default value specified.
+func defaultInt64(i *int64, def int64) int64 {
+	if i != nil {
+		return *i
+	}
+
+	return def
+}
+
 // nonEmptyStringPtr returns a non-nil pointer to s if the string is not empty, otherwise nil.
 func nonEmptyStringPtr(s string) *string {
 	if s != "" {
