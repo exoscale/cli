c4c1d233cbc7b4b79fd5b63c31dd9019497eafd5
Philipp Sauter <philipp.sauter@exoscale.ch>
2023-02-24 16:12:23 +0100

    output instances as list in JSON

--- cmd/elastic_ip_show.go
+++ cmd/elastic_ip_show.go
@@ -22,7 +22,7 @@ type elasticIPShowOutput struct {
 	Zone                     string         `json:"zone"`
 	Type                     string         `json:"type"`
 	ReverseDNS               string         `json:"reverse_dns"`
-	Instances                string         `json:"instances"`
+	Instances                []string       `json:"instances"`
 	HealthcheckMode          *string        `json:"healthcheck_mode,omitempty"`
 	HealthcheckPort          *uint16        `json:"healthcheck_port,omitempty"`
 	HealthcheckURI           *string        `json:"healthcheck_uri,omitempty"`
@@ -49,7 +49,12 @@ func (o *elasticIPShowOutput) toTable() {
 	t.Append([]string{"Zone", o.Zone})
 	t.Append([]string{"Type", o.Type})
 	t.Append([]string{"Reverse DNS", o.ReverseDNS})
-	t.Append([]string{"Instances", o.Instances})
+
+	instances := ""
+	for _, instance := range o.Instances {
+		instances += instance + " "
+	}
+	t.Append([]string{"Instances", instances})
 
 	if o.Type == "managed" {
 		t.Append([]string{"Healthcheck Mode", *o.HealthcheckMode})
@@ -135,7 +140,7 @@ func (c *elasticIPShowCmd) cmdRun(_ *cobra.Command, _ []string) error {
 	}
 
 	for _, instance := range attachedInstances {
-		out.Instances += *instance.Name + " "
+		out.Instances = append(out.Instances, *instance.Name)
 	}
 
 	if elasticIP.Healthcheck != nil {
