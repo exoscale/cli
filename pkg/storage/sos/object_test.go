6e8f7f43803f2c66740d0717985f887e88928e73
Philipp Sauter <philipp.sauter@exoscale.ch>
2023-05-03 17:59:48 +0200

    test for DeleteObjects

--- pkg/storage/sos/object_test.go
+++ pkg/storage/sos/object_test.go
@@ -7,7 +7,9 @@ import (
 	"testing"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/exoscale/cli/pkg/storage/sos"
 	"github.com/stretchr/testify/assert"
 )
@@ -88,4 +90,77 @@ func TestShowObject(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestClient_DeleteObjects(t *testing.T) {
+	bucket := "test-bucket"
+	commonPrefix := "myobjects/"
+	objectKeys := []string{commonPrefix + "object1", commonPrefix + "object2", commonPrefix + "object3"}
+
+	nCalls := 0
+	expectedDeleteInput := &s3.DeleteObjectsInput{
+		Bucket: &bucket,
+		Delete: &types.Delete{
+			Objects: []types.ObjectIdentifier{
+				{Key: aws.String(objectKeys[0])},
+				{Key: aws.String(objectKeys[1])},
+				{Key: aws.String(objectKeys[2])},
+			},
+		},
+	}
+	client := sos.Client{
+		S3Client: &MockS3API{
+			mockDeleteObjects: func(ctx context.Context, params *s3.DeleteObjectsInput, optFns ...func(*s3.Options)) (*s3.DeleteObjectsOutput, error) {
+				nCalls++
+				assert.Equal(t, expectedDeleteInput, params)
+				return &s3.DeleteObjectsOutput{
+					Deleted: []types.DeletedObject{
+						{Key: aws.String(objectKeys[0])},
+						{Key: aws.String(objectKeys[1])},
+						{Key: aws.String(objectKeys[2])},
+					},
+				}, nil
+			},
+			mockListObjectsV2: func(ctx context.Context, params *s3.ListObjectsV2Input, optFns ...func(*s3.Options)) (*s3.ListObjectsV2Output, error) {
+				return &s3.ListObjectsV2Output{
+					IsTruncated: false,
+					Contents: []types.Object{
+						{Key: aws.String(objectKeys[0])},
+						{Key: aws.String(objectKeys[1])},
+						{Key: aws.String(objectKeys[2])},
+					},
+				}, nil
+			},
+		},
+	}
+
+	deleted, err := client.DeleteObjects(context.Background(), bucket, commonPrefix, false)
+	assert.NoError(t, err)
+	assert.Equal(t, 3, len(deleted))
+	assert.Equal(t, 1, nCalls)
+
+	for i, key := range deleted {
+		assert.Equal(t, objectKeys[i], *key.Key)
+	}
+
+	client = sos.Client{
+		S3Client: &MockS3API{
+			mockDeleteObjects: func(ctx context.Context, params *s3.DeleteObjectsInput, optFns ...func(*s3.Options)) (*s3.DeleteObjectsOutput, error) {
+				return nil, errors.New("delete error")
+			},
+			mockListObjectsV2: func(ctx context.Context, params *s3.ListObjectsV2Input, optFns ...func(*s3.Options)) (*s3.ListObjectsV2Output, error) {
+				return &s3.ListObjectsV2Output{
+					IsTruncated: false,
+					Contents: []types.Object{
+						{
+							Key: aws.String("some-file"),
+						},
+					},
+				}, nil
+			},
+		},
+	}
+
+	_, err = client.DeleteObjects(context.Background(), bucket, commonPrefix, false)
+	assert.Error(t, err)
 }
