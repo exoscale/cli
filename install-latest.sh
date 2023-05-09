680115b3a01efe381833d0f902bf3c7d6b1bd46c
Philipp Sauter <philipp.sauter@exoscale.ch>
2023-05-09 17:43:07 +0200

    fix bad substitution

--- install-latest.sh
+++ install-latest.sh
@@ -8,7 +8,7 @@ set -eu
 # detect the latest version of the exoscale cli
 LATEST_URL=$(curl -L -s -o /dev/null -w %{url_effective} https://github.com/exoscale/cli/releases/latest)
 LATEST_TAG=$(basename "${LATEST_URL}")
-LATEST_VERSION="${LATEST_TAG:1}"
+LATEST_VERSION=$(echo "$LATEST_TAG" | cut -c 2-)
 
 OS=""
 OSTYPE=""
@@ -19,33 +19,32 @@ PACKAGETYPE=""
 APT_KEY_TYPE="" # Only for apt-based distros
 
 if [ -f /etc/os-release ]; then
-	. /etc/os-release
+    . /etc/os-release
     case "$ID" in
-		debian|ubuntu|pop|neon|zorin|linuxmint|elementary|parrot|mendel|galliumos|pureos|raspian|kali|Deepin)
-			OS="$ID"
-			PACKAGETYPE="deb"
+        debian | ubuntu | pop | neon | zorin | linuxmint | elementary | parrot | mendel | galliumos | pureos | raspian | kali | Deepin)
+            OS="$ID"
+            PACKAGETYPE="deb"
             FILEEXT="deb"
-			;;
-		fedora)
-			OS="$ID"
-			VERSION=""
-			PACKAGETYPE="dnf"
+            ;;
+        fedora)
+            OS="$ID"
+            VERSION=""
+            PACKAGETYPE="dnf"
             FILEEXT="rpm"
             OSTYPE="linux"
-			;;
-		centos)
-			OS="$ID"
-			VERSION="$VERSION_ID"
-			PACKAGETYPE="dnf"
+            ;;
+        centos)
+            OS="$ID"
+            VERSION="$VERSION_ID"
+            PACKAGETYPE="dnf"
             FILEEXT="rpm"
             OSTYPE="linux"
-			if [ "$VERSION" = "7" ]; then
-				PACKAGETYPE="yum"
-			fi
-			;;
+            if [ "$VERSION" = "7" ]; then
+                PACKAGETYPE="yum"
+            fi
+            ;;
     esac
 fi
-
 
 # detect the cpu architecture
 CPUARCHITECTURE="$(uname -m)"
@@ -62,51 +61,51 @@ esac
 # only have wget. Detect and use what's available.
 CURL=
 if type curl >/dev/null; then
-	CURL="curl -fsSL"
+    CURL="curl -fsSL"
 elif type wget >/dev/null; then
-	CURL="wget -q -O-"
+    CURL="wget -q -O-"
 fi
 if [ -z "$CURL" ]; then
-	echo "The installer needs either curl or wget to download files."
-	echo "Please install either curl or wget to proceed."
-	exit 1
+    echo "The installer needs either curl or wget to download files."
+    echo "Please install either curl or wget to proceed."
+    exit 1
 fi
 
 TEST_URL="https://exoscale.com/"
 RC=0
 TEST_OUT=$($CURL "$TEST_URL" 2>&1) || RC=$?
 if [ $RC != 0 ]; then
-	echo "The installer cannot reach $TEST_URL"
-	echo "Please make sure that your machine has internet access."
-	echo "Test output:"
-	echo $TEST_OUT
-	exit 1
+    echo "The installer cannot reach $TEST_URL"
+    echo "Please make sure that your machine has internet access."
+    echo "Test output:"
+    echo $TEST_OUT
+    exit 1
 fi
 
 # work out if we can run privileged commands, and if so, how
 CAN_ROOT=
 SUDO=
 if [ "$(id -u)" = 0 ]; then
-	CAN_ROOT=1
-	SUDO=""
+    CAN_ROOT=1
+    SUDO=""
 elif type sudo >/dev/null; then
-	CAN_ROOT=1
-	SUDO="sudo"
+    CAN_ROOT=1
+    SUDO="sudo"
 elif type doas >/dev/null; then
-	CAN_ROOT=1
-	SUDO="doas"
+    CAN_ROOT=1
+    SUDO="doas"
 fi
 if [ "$CAN_ROOT" != "1" ]; then
-	echo "This installer needs to run commands as root."
-	echo "We tried looking for 'sudo' and 'doas', but couldn't find them."
-	echo "Either re-run this script as root, or set up sudo/doas."
-	exit 1
+    echo "This installer needs to run commands as root."
+    echo "We tried looking for 'sudo' and 'doas', but couldn't find them."
+    echo "Either re-run this script as root, or set up sudo/doas."
+    exit 1
 fi
 
 TEMPDIR=$(mktemp -d)
 PKGFILE="exoscale-cli_${LATEST_VERSION}_${OSTYPE}_${CPUARCHITECTURE}.${FILEEXT}"
 PKGPATH=$TEMPDIR/$PKGFILE
-$CURL "https://github.com/exoscale/cli/releases/download/${LATEST_TAG}/$PKGFILE" > $PKGPATH
+$CURL "https://github.com/exoscale/cli/releases/download/${LATEST_TAG}/$PKGFILE" >$PKGPATH
 
 OSVERSION="$OS"
 echo "Installing exo CLI for $OSVERSION, using method $PACKAGETYPE"
@@ -114,16 +113,16 @@ case "$PACKAGETYPE" in
     deb)
         set -x
         $SUDO dpkg -i $PKGPATH
-		set -x
-    ;;
-	yum)
-		set -x
-		$SUDO yum install -y $PKGPATH
-		set +x
-	;;
-	dnf)
-		set -x
-		$SUDO dnf install -y $PKGPATH
-		set +x
-	;;
+        set -x
+        ;;
+    yum)
+        set -x
+        $SUDO yum install -y $PKGPATH
+        set +x
+        ;;
+    dnf)
+        set -x
+        $SUDO dnf install -y $PKGPATH
+        set +x
+        ;;
 esac
