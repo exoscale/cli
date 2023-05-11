73b5bbeb96a2f3651d2daf3e08b54c6819a5a6bc
Philipp Sauter <46172817+sauterp@users.noreply.github.com>
2023-05-11 16:59:15 +0200

    installation: add a script to install the latest version on Debian and Red Hat-based distros. (#505)
    In the discussion of #492 it became apparent that some users are lacking
    an easy and automatic way to install the latest version of exo cli. We
    add a script to automate this for Debian and Red Hat-based
    distributions.
    
    The script requires `sh`, `sha256sum`(to check the checksum), `curl` or
    `wget` and either `dpkg`, `yum` or `dnf`. It detects automatically which
    one of these commands should be used to download and install the
    package.
    
    The script can be run in one command with `curl -fsSL <url to script> |
    sh` and apart from invoking `sudo` or `doas` it should not require any
    more user interaction.
    
    ---------
    
    Co-authored-by: Philipp Sauter <philipp.sauter@exoscale.ch>

---
+++ install-latest.sh
@@ -0,0 +1,149 @@
+#!/usr/bin/env sh
+
+# a lot of inspiration for this installer was taken from the tailscale installer script at https://github.com/tailscale/tailscale/blob/827abbeeaaf21388ec0dc7046d61d8350afe98f7/scripts/installer.sh
+
+# exit if one command exits with non-zero exit code and treat unset variables as an error
+set -eu
+
+# detect the latest version of the exoscale cli
+LATEST_URL=$(curl -L -s -o /dev/null -w %{url_effective} https://github.com/exoscale/cli/releases/latest)
+LATEST_TAG=$(basename "${LATEST_URL}")
+LATEST_VERSION=$(echo "$LATEST_TAG" | cut -c 2-)
+
+OSTYPE=""
+CPUARCHITECTURE=""
+FILEEXT=""
+PACKAGETYPE=""
+
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    case "$ID" in
+        debian | ubuntu | pop | neon | zorin | linuxmint | elementary | parrot | mendel | galliumos | pureos | raspian | kali | Deepin)
+            PACKAGETYPE="dpkg"
+            FILEEXT="deb"
+            OSTYPE="linux"
+            ;;
+        fedora | rocky | almalinux | nobara | openmandriva | sangoma | risios)
+            PACKAGETYPE="dnf"
+            FILEEXT="rpm"
+            OSTYPE="linux"
+            ;;
+        rhel)
+            VERSION="$(echo "$VERSION_ID" | cut -f1 -d.)"
+            PACKAGETYPE="dnf"
+            FILEEXT="rpm"
+            if [ "$VERSION" = "7" ]; then
+                PACKAGETYPE="yum"
+            fi
+            ;;
+        centos)
+            PACKAGETYPE="dnf"
+            FILEEXT="rpm"
+            OSTYPE="linux"
+            if [ "$VERSION_ID" = "7" ]; then
+                PACKAGETYPE="yum"
+            fi
+            ;;
+        amzn | xenenterprise)
+            PACKAGETYPE="yum"
+            FILEEXT="rpm"
+            OSTYPE="linux"
+            ;;
+        *)
+            echo "your OS is not supported by this script, please install manually https://community.exoscale.com/documentation/tools/exoscale-command-line-interface/#installation"
+            exit 1
+            ;;
+    esac
+fi
+
+# detect the cpu architecture
+CPUARCHITECTURE="$(uname -m)"
+case "$(uname -m)" in
+    x86_64)
+        CPUARCHITECTURE="amd64"
+        ;;
+    armv7l)
+        CPUARCHITECTURE="armv7"
+        ;;
+esac
+
+# Ideally we want to use curl, but on some installs we
+# only have wget. Detect and use what's available.
+CURL=
+if type curl >/dev/null; then
+    CURL="curl -fsSL"
+elif type wget >/dev/null; then
+    CURL="wget -q -O-"
+fi
+if [ -z "$CURL" ]; then
+    echo "The installer needs either curl or wget to download files."
+    echo "Please install either curl or wget to proceed."
+    exit 1
+fi
+
+TEST_URL="https://www.exoscale.com/"
+RC=0
+TEST_OUT=$($CURL "$TEST_URL" 2>&1) || RC=$?
+if [ $RC != 0 ]; then
+    echo "The installer cannot reach $TEST_URL"
+    echo "Please make sure that your machine has internet access."
+    echo "Test output:"
+    echo $TEST_OUT
+    exit 1
+fi
+
+# work out if we can run privileged commands, and if so, how
+CAN_ROOT=
+SUDO=
+if [ "$(id -u)" = 0 ]; then
+    CAN_ROOT=1
+    SUDO=""
+elif type sudo >/dev/null; then
+    CAN_ROOT=1
+    SUDO="sudo"
+elif type doas >/dev/null; then
+    CAN_ROOT=1
+    SUDO="doas"
+fi
+if [ "$CAN_ROOT" != "1" ]; then
+    echo "This installer needs to run commands as root."
+    echo "We tried looking for 'sudo' and 'doas', but couldn't find them."
+    echo "Either re-run this script as root, or set up sudo/doas."
+    exit 1
+fi
+
+GITHUB_DOWNLOAD_URL="https://github.com/exoscale/cli/releases/download"
+
+TEMPDIR=$(mktemp -d)
+PKGPREFIX="exoscale-cli"
+PKGFILE="${PKGPREFIX}_${LATEST_VERSION}_${OSTYPE}_${CPUARCHITECTURE}.${FILEEXT}"
+PKGPATH=$TEMPDIR/$PKGFILE
+$CURL "$GITHUB_DOWNLOAD_URL/${LATEST_TAG}/$PKGFILE" >$PKGPATH
+
+# check the checksum
+CHECKSUMSFILE="${PKGPREFIX}_${LATEST_VERSION}_checksums.txt"
+CHECKSUMSPATH=$TEMPDIR/$CHECKSUMSFILE
+$CURL "$GITHUB_DOWNLOAD_URL/${LATEST_TAG}/$CHECKSUMSFILE" >$CHECKSUMSPATH
+
+COMPUTED_CHECKSUM=$(sha256sum "$PKGPATH" | cut -d " " -f 1)
+EXPECTED_CHECKSUM=$(grep -m 1 $PKGFILE $CHECKSUMSPATH | cut -d " " -f 1)
+
+if [ "$COMPUTED_CHECKSUM" != "$EXPECTED_CHECKSUM" ]; then
+    echo "Error: Checksum of $PKGFILE does not match the expected checksum"
+    echo $COMPUTED_CHECKSUM
+    echo $EXPECTED_CHECKSUM
+    exit 1
+fi
+
+echo "Installing exo CLI, using $PACKAGETYPE"
+case "$PACKAGETYPE" in
+    dpkg)
+        $SUDO dpkg -i $PKGPATH
+        ;;
+    yum)
+        $SUDO yum install -y $PKGPATH
+        ;;
+    dnf)
+        $SUDO dnf install -y $PKGPATH
+        ;;
+esac
