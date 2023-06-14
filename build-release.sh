9df407b87682c6661297353586acaeb1cedd876f
Philipp Sauter <philipp.sauter@exoscale.ch>
2023-06-14 14:08:08 +0200

    release works

--- build-release.sh
+++ build-release.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env sh
 
+# to avoid permission errors we mount the /src folder read-only and create a snapshot
 SNAPSHOT_DIR=/snapshot
-rsync -rltD --delete /src/ $SNAPSHOT_DIR/
+
+rsync \
+    --recursive \
+    --links \
+    --times \
+    --delete \
+    --exclude=/dist/ \
+    /src/ $SNAPSHOT_DIR/
 
 cd $SNAPSHOT_DIR
 
+# TODO(sauterp) remove
+git clean -f
+git reset --hard HEAD
+
 export CGO_ENABLED=1
-make test
+make release
