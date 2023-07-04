9110deb75609933ca5bdce69bee478201a8d46e4
Philipp Sauter <philipp.sauter@exoscale.ch>
2023-07-04 18:45:35 +0200

    fix

--- aptly-release.sh
+++ aptly-release.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-aptly -config=.aptly.conf repo create my-first-repo
-aptly -config=.aptly.conf repo import s3:sauterp-aptly-test-repo: my-first-repo
-aptly -config=.aptly.conf repo add my-first-repo $1
+reponame=myrepo
+
+aptly -config=.aptly.conf repo create $reponame
+aptly -config=.aptly.conf repo add $reponame $1
 aptly -config=.aptly.conf publish update ubuntu s3:sauterp-aptly-test-repo:
