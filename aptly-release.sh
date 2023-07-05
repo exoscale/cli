570ba5cbe134df6954dfb62b850b5f20519ca711
Philipp Sauter <philipp.sauter@exoscale.ch>
2023-07-05 13:37:33 +0200

    no signing

--- aptly-release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-set -e
-
-reponame=myrepo
-
-aptly -config=.aptly.conf repo create $reponame
-aptly -config=.aptly.conf repo add $reponame $1
-aptly -config=.aptly.conf publish update ubuntu s3:sauterp-aptly-test-repo:
