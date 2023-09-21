e51b22da510e3a571e7f2a572198ea118c6d6ba9
Philipp Sauter <philipp.sauter@exoscale.ch>
2023-09-21 12:33:56 +0200

    wip

--- publish-artifact.sh
+++ publish-artifact.sh
@@ -11,28 +11,35 @@ aptlydistro=stable
 aptlycmd="aptly -config=.aptly.conf"
 gpgkeyflag='-gpg-key=7100E8BFD6199CE0374CB7F003686F8CDE378D41'
 
-if $aptlycmd mirror show $aptlymirror >/dev/null 2>&1; then
-    echo "repo $aptlymirror already exists"
-else
-    $aptlycmd mirror create \
-        $aptlymirror \
-        $archiveurl \
-        $aptlydistro
-fi
+# if $aptlycmd mirror show $aptlymirror >/dev/null 2>&1; then
+#     echo "repo $aptlymirror already exists"
+# else
+#     $aptlycmd mirror create \
+#         -filter="(latest)" \
+#         $aptlymirror \
+#         $archiveurl \
+#         $aptlydistro
+# fi
 
-$aptlycmd mirror update \
-    $aptlymirror
+# $aptlycmd mirror update \
+#     $aptlymirror
 
 if $aptlycmd repo show $aptlyrepo >/dev/null 2>&1; then
     echo "repo $aptlyrepo already exists"
 else
-    $aptlycmd repo import $aptlymirror $aptlyrepo $aptlydistro
+    $aptlycmd repo create $aptlyrepo
+    # $aptlycmd repo import $aptlymirror $aptlyrepo $aptlydistro
 fi
 
 if echo $artifact | grep -q '.*.deb'; then
     $aptlycmd repo add $aptlyrepo $artifact
-    $aptlycmd publish update \
+    $aptlycmd publish repo \
         $gpgkeyflag \
-        $aptlydistro \
+        -distribution=$aptlydistro \
+        $aptlyrepo \
         $aptlyremote
+    # $aptlycmd publish update \
+    #     $gpgkeyflag \
+    #     $aptlydistro \
+    #     $aptlyremote
 fi
