6245396e0c0b981f7f99038cc9cf46362880e19e
Predrag Janosevic <predrag.janosevic@exoscale.com>
2023-11-20 15:22:53 +0100

    Add iam role create command

---
+++ cmd/iam_role_create.go
@@ -0,0 +1,144 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	"github.com/exoscale/cli/pkg/account"
+	"github.com/exoscale/cli/pkg/globalstate"
+	"github.com/exoscale/cli/pkg/output"
+	exoscale "github.com/exoscale/egoscale/v2"
+	exoapi "github.com/exoscale/egoscale/v2/api"
+)
+
+type iamRoleCreateCmd struct {
+	cliCommandSettings `cli-cmd:"-"`
+
+	_ bool `cli-cmd:"create"`
+
+	Name        string            `cli-flag:"name" cli-usage:"Role name (required)"`
+	Description string            `cli-flag:"description" cli-usage:"Role description"`
+	Permissions []string          `cli-flag:"permissions" cli-usage:"Role permissions"`
+	Editable    bool              `cli-flag:"editable" cli-usage:"Sets if the Role Policy is editable or not (default: true). This setting cannot be changed after creation"`
+	Labels      map[string]string `cli-flag:"label" cli-usage:"Role labels (format: key=value)"`
+	Policy      string            `cli-flag:"policy" cli-usage:"Role policy (use '-' to read from STDIN)"`
+}
+
+func (c *iamRoleCreateCmd) cmdAliases() []string { return nil }
+
+func (c *iamRoleCreateCmd) cmdShort() string {
+	return "Create IAM Role"
+}
+
+func (c *iamRoleCreateCmd) cmdLong() string {
+	return fmt.Sprintf(`This command creates a new IAM Role.
+To read Policy from STDIN use '-' for policy.
+
+Pro Tip: you can clone existing role by providing output of show command as Policy:
+
+	exo iam role show --policy --output-format json <policy-name> | exo iam role create --name <cloned-policy-name> --policy -
+
+Supported output template annotations: %s`,
+		strings.Join(output.TemplateAnnotations(&iamRoleShowOutput{}), ", "))
+}
+
+func (c *iamRoleCreateCmd) cmdPreRun(cmd *cobra.Command, args []string) error {
+	return cliCommandDefaultPreRun(c, cmd, args)
+}
+
+func (c *iamRoleCreateCmd) cmdRun(cmd *cobra.Command, _ []string) error {
+	zone := account.CurrentAccount.DefaultZone
+	ctx := exoapi.WithEndpoint(
+		gContext,
+		exoapi.NewReqEndpoint(account.CurrentAccount.Environment, zone),
+	)
+
+	if c.Policy == "-" {
+		inputReader := cmd.InOrStdin()
+		b, err := io.ReadAll(inputReader)
+		if err != nil {
+			return fmt.Errorf("failed to read policy from stdin: %w", err)
+		}
+
+		c.Policy = string(b)
+	}
+
+	var obj iamPolicyOutput
+	err := json.Unmarshal([]byte(c.Policy), &obj)
+	if err != nil {
+		return fmt.Errorf("failed to parse policy: %w", err)
+	}
+
+	policy := &exoscale.IAMPolicy{
+		DefaultServiceStrategy: obj.DefaultServiceStrategy,
+		Services:               map[string]exoscale.IAMPolicyService{},
+	}
+
+	if len(obj.Services) > 0 {
+		for name, sv := range obj.Services {
+			service := exoscale.IAMPolicyService{
+				Type: func() *string {
+					t := sv.Type
+					return &t
+				}(),
+			}
+
+			if len(sv.Rules) > 0 {
+				service.Rules = []exoscale.IAMPolicyServiceRule{}
+				for _, rl := range sv.Rules {
+
+					rule := exoscale.IAMPolicyServiceRule{
+						Action: func() *string {
+							t := rl.Action
+							return &t
+						}(),
+					}
+
+					if rl.Expression != "" {
+						rule.Expression = func() *string {
+							t := rl.Expression
+							return &t
+						}()
+					}
+
+					service.Rules = append(service.Rules, rule)
+				}
+			}
+
+			policy.Services[name] = service
+		}
+	}
+
+	role := &exoscale.IAMRole{
+		Name:        &c.Name,
+		Editable:    &c.Editable,
+		Labels:      c.Labels,
+		Permissions: c.Permissions,
+		Policy:      policy,
+	}
+
+	r, err := globalstate.EgoscaleClient.CreateIAMRole(ctx, zone, role)
+	if err != nil {
+		return err
+	}
+
+	if !globalstate.Quiet {
+		return (&iamRoleShowCmd{
+			cliCommandSettings: c.cliCommandSettings,
+			Role:               *r.ID,
+		}).cmdRun(nil, nil)
+	}
+
+	return nil
+}
+
+func init() {
+	cobra.CheckErr(registerCLICommand(iamRoleCmd, &iamRoleCreateCmd{
+		cliCommandSettings: defaultCLICmdSettings(),
+		Editable:           true,
+	}))
+}
