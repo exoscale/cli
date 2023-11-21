319ff62d16f5893f8dc5499927d55a9391422b29
Predrag Janosevic <predrag.janosevic@exoscale.com>
2023-11-21 13:16:08 +0100

    Implement API Key list command

---
+++ cmd/iam_api_key.go
@@ -0,0 +1,14 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var iamAPIKeyCmd = &cobra.Command{
+	Use:   "api-key",
+	Short: "API Key management",
+}
+
+func init() {
+	iamCmd.AddCommand(iamAPIKeyCmd)
+}
