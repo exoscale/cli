5f15972895b35854341645474db695a93382a6d9
Predrag Janosevic <predrag.janosevic@exoscale.com>
2024-01-08 17:06:52 +0100

    iam role create: fix description and policy when not set (optional)

--- cmd/iam_role_create.go
+++ cmd/iam_role_create.go
@@ -41,7 +41,7 @@ To read the Policy from STDIN, append '-' to the '--policy' flag.
 
 Pro Tip: you can reuse an existing role policy by providing the output of the show command as input:
 
-	exo iam role show --policy --output-format json <role-name> | exo iam role create <new-role-name> --description "new role description" --policy -
+	exo iam role show --policy --output-format json <role-name> | exo iam role create <new-role-name> --policy -
 
 Supported output template annotations: %s`,
 		strings.Join(output.TemplateAnnotations(&iamRoleShowOutput{}), ", "))
@@ -62,69 +62,78 @@ func (c *iamRoleCreateCmd) cmdRun(cmd *cobra.Command, _ []string) error {
 		exoapi.NewReqEndpoint(account.CurrentAccount.Environment, zone),
 	)
 
-	if c.Policy == "-" {
-		inputReader := cmd.InOrStdin()
-		b, err := io.ReadAll(inputReader)
-		if err != nil {
-			return fmt.Errorf("failed to read policy from stdin: %w", err)
+	var policy *exoscale.IAMPolicy
+
+	// Policy is optional, if not set API will default to `allow all`
+	if c.Policy != "" {
+		// If Policy value is `-` read from STDIN
+		if c.Policy == "-" {
+			inputReader := cmd.InOrStdin()
+			b, err := io.ReadAll(inputReader)
+			if err != nil {
+				return fmt.Errorf("failed to read policy from stdin: %w", err)
+			}
+
+			c.Policy = string(b)
 		}
 
-		c.Policy = string(b)
-	}
+		var obj iamPolicyOutput
+		err := json.Unmarshal([]byte(c.Policy), &obj)
+		if err != nil {
+			return fmt.Errorf("failed to parse policy: %w", err)
+		}
 
-	var obj iamPolicyOutput
-	err := json.Unmarshal([]byte(c.Policy), &obj)
-	if err != nil {
-		return fmt.Errorf("failed to parse policy: %w", err)
-	}
+		policy = &exoscale.IAMPolicy{
+			DefaultServiceStrategy: obj.DefaultServiceStrategy,
+			Services:               map[string]exoscale.IAMPolicyService{},
+		}
 
-	policy := &exoscale.IAMPolicy{
-		DefaultServiceStrategy: obj.DefaultServiceStrategy,
-		Services:               map[string]exoscale.IAMPolicyService{},
-	}
-
-	if len(obj.Services) > 0 {
-		for name, sv := range obj.Services {
-			service := exoscale.IAMPolicyService{
-				Type: func() *string {
-					t := sv.Type
-					return &t
-				}(),
-			}
-
-			if len(sv.Rules) > 0 {
-				service.Rules = []exoscale.IAMPolicyServiceRule{}
-				for _, rl := range sv.Rules {
-
-					rule := exoscale.IAMPolicyServiceRule{
-						Action: func() *string {
-							t := rl.Action
-							return &t
-						}(),
-					}
-
-					if rl.Expression != "" {
-						rule.Expression = func() *string {
-							t := rl.Expression
-							return &t
-						}()
-					}
-
-					service.Rules = append(service.Rules, rule)
+		if len(obj.Services) > 0 {
+			for name, sv := range obj.Services {
+				service := exoscale.IAMPolicyService{
+					Type: func() *string {
+						t := sv.Type
+						return &t
+					}(),
 				}
-			}
 
-			policy.Services[name] = service
+				if len(sv.Rules) > 0 {
+					service.Rules = []exoscale.IAMPolicyServiceRule{}
+					for _, rl := range sv.Rules {
+
+						rule := exoscale.IAMPolicyServiceRule{
+							Action: func() *string {
+								t := rl.Action
+								return &t
+							}(),
+						}
+
+						if rl.Expression != "" {
+							rule.Expression = func() *string {
+								t := rl.Expression
+								return &t
+							}()
+						}
+
+						service.Rules = append(service.Rules, rule)
+					}
+				}
+
+				policy.Services[name] = service
+			}
 		}
 	}
 
 	role := &exoscale.IAMRole{
 		Name:        &c.Name,
-		Description: &c.Description,
 		Editable:    &c.Editable,
 		Labels:      c.Labels,
 		Permissions: c.Permissions,
 		Policy:      policy,
+	}
+
+	if c.Description != "" {
+		role.Description = &c.Description
 	}
 
 	r, err := globalstate.EgoscaleClient.CreateIAMRole(ctx, zone, role)
