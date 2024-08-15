dda1243406dac95bc3f4ef08ab3a3f461810e2e5
elkezza <47871727+elkezza@users.noreply.github.com>
2024-08-15 17:02:43 +0200

    running goimports.

--- cmd/instance_pool_create.go
+++ cmd/instance_pool_create.go
@@ -2,8 +2,9 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
 	"strings"
+
+	"github.com/spf13/cobra"
 
 	"github.com/exoscale/cli/pkg/account"
 	"github.com/exoscale/cli/pkg/globalstate"
