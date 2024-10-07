2fc86933621f557b5647269137e186bb8bee807e
simisoft-exo <simion@simisoft.co.uk>
2024-10-07 12:21:53 +0100

    show datadog settings

---
+++ cmd/dbaas_external_integration_settings_show.go
@@ -0,0 +1,36 @@
+package cmd
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+type dbaasExternalIntegrationSettingsShowCmd struct {
+	cliCommandSettings `cli-cmd:"-"`
+
+	_ bool `cli-cmd:"show"`
+
+	Type          string `cli-arg:"#"`
+	IntegrationID string `cli-arg:"#"`
+}
+
+func (c *dbaasExternalIntegrationSettingsShowCmd) cmdAliases() []string { return gShowAlias }
+func (c *dbaasExternalIntegrationSettingsShowCmd) cmdShort() string { return "Show External Integration Settings"}
+func (c *dbaasExternalIntegrationSettingsShowCmd) cmdLong() string { return "Show External Integration Settings"}
+func (c *dbaasExternalIntegrationSettingsShowCmd) cmdPreRun(cmd *cobra.Command, args []string) error {
+	return cliCommandDefaultPreRun(c, cmd, args)
+}
+
+func (c *dbaasExternalIntegrationSettingsShowCmd) cmdRun(_ *cobra.Command, _ []string) error {
+	switch c.Type {
+	case "datadog":
+		return c.outputFunc(c.showDatadog())
+	default:
+		return fmt.Errorf("unsupported external integration settings for type %q", c.Type)
+	}
+}
+
+func init() {
+	cobra.CheckErr(registerCLICommand(dbaasExternalIntegrationSettingsCmd, &dbaasExternalIntegrationSettingsShowCmd{
+		cliCommandSettings: defaultCLICmdSettings(),
+	}))
+}
---
+++ cmd/dbaas_external_integration_settings_show_datadog.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/exoscale/cli/pkg/account"
+	"github.com/exoscale/cli/pkg/globalstate"
+	"github.com/exoscale/cli/pkg/output"
+	v3 "github.com/exoscale/egoscale/v3"
+)
+
+type dbaasExternalIntegrationSettingsShowDatadogOutput struct {
+	DatadogDbmEnabled bool `json:"datadog-dbm-enabled"`
+	DatadogPgbouncerEnabled bool `json:"datadog-pgbouncer-enabled"`
+}
+
+func (o *dbaasExternalIntegrationSettingsShowDatadogOutput) ToJSON()  { output.JSON(o) }
+func (o *dbaasExternalIntegrationSettingsShowDatadogOutput) ToText()  { output.Text(o) }
+func (o *dbaasExternalIntegrationSettingsShowDatadogOutput) ToTable() { output.Table(o) }
+
+func (c *dbaasExternalIntegrationSettingsShowCmd) showDatadog() (output.Outputter, error) {
+	ctx := gContext
+
+	client, err := switchClientZoneV3(ctx, globalstate.EgoscaleV3Client, v3.ZoneName(account.CurrentAccount.DefaultZone))
+	if err != nil {
+		return nil, err
+	}
+
+	integrationID, err := v3.ParseUUID(c.IntegrationID)
+	if err != nil {
+		return nil, fmt.Errorf("invalid integration ID: %w", err)
+	}
+
+	res, err := client.GetDBAASExternalIntegrationSettingsDatadog(ctx, integrationID)
+	if err != nil {
+		return nil, fmt.Errorf("error getting settings for integration: %w", err)
+	}
+
+	out := &dbaasExternalIntegrationSettingsShowDatadogOutput{
+		DatadogDbmEnabled: *res.Settings.DatadogDbmEnabled,
+		DatadogPgbouncerEnabled: *res.Settings.DatadogPgbouncerEnabled,
+	}
+	return out, nil
+}
