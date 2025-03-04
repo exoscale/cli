c391f5d38542713c76b1a863c25b3a9cb4912fd8
Your Name <you@example.com>
2025-03-04 12:07:15 +0100

    fix versions.

--- cmd/sks_versions.go
+++ cmd/sks_versions.go
@@ -60,7 +60,7 @@ func (c *sksVersionsCmd) cmdRun(_ *cobra.Command, _ []string) error {
 	}
 
 	for _, v := range versions.SKSClusterVersions {
-		_ = append(out, sksClusterVersionsItemOutput{Version: v})
+		out = append(out, sksClusterVersionsItemOutput{Version: v})
 	}
 
 	return c.outputFunc(&out, nil)
