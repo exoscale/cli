1a12774909de15964379364b6781e6e7117f054b
Your Name <you@example.com>
2025-03-06 13:53:20 +0100

    exctract nodepool creation logic into nodepool.go.

--- cmd/sks_nodepool_add.go
+++ cmd/sks_nodepool_add.go
@@ -61,19 +61,6 @@ func (c *sksNodepoolAddCmd) cmdPreRun(cmd *cobra.Command, args []string) error {
 }
 
 func (c *sksNodepoolAddCmd) cmdRun(_ *cobra.Command, _ []string) error {
-	nodepoolReq := v3.CreateSKSNodepoolRequest{
-		Description:    c.Description,
-		DiskSize:       c.DiskSize,
-		InstancePrefix: c.InstancePrefix,
-		Name:           c.Name,
-		Size:           c.Size,
-		KubeletImageGC: &v3.KubeletImageGC{
-			MinAge:        c.ImageGcMinAge,
-			LowThreshold:  c.ImageGcLowThreshold,
-			HighThreshold: c.ImageGcHighThreshold,
-		},
-	}
-
 	ctx := gContext
 
 	client, err := switchClientZoneV3(ctx, globalstate.EgoscaleV3Client, v3.ZoneName(c.Zone))
@@ -91,6 +78,39 @@ func (c *sksNodepoolAddCmd) cmdRun(_ *cobra.Command, _ []string) error {
 		return err
 	}
 
+	labels := make(map[string]string)
+	if len(c.Labels) > 0 {
+		labels, err = utils.SliceToMap(c.Labels)
+		if err != nil {
+			return fmt.Errorf("label: %w", err)
+		}
+	}
+
+	nodepoolReq, err := createNodepoolRequest(
+		ctx,
+		client,
+		c.Name,
+		c.Description,
+		c.DiskSize,
+		c.InstancePrefix,
+		c.Size,
+		c.InstanceType,
+		labels,
+		c.AntiAffinityGroups,
+		c.DeployTarget,
+		c.PrivateNetworks,
+		c.SecurityGroups,
+		c.Taints,
+		&v3.KubeletImageGC{
+			MinAge:        c.ImageGcMinAge,
+			LowThreshold:  c.ImageGcLowThreshold,
+			HighThreshold: c.ImageGcHighThreshold,
+		},
+	)
+	if err != nil {
+		return err
+	}
+
 	addOns := map[string]bool{
 		"storage-lvm": c.StorageLvm,
 	}
@@ -100,98 +120,6 @@ func (c *sksNodepoolAddCmd) cmdRun(_ *cobra.Command, _ []string) error {
 		if v {
 			nodepoolReq.Addons = append(nodepoolReq.Addons, k)
 		}
-	}
-
-	if l := len(c.AntiAffinityGroups); l > 0 {
-		nodepoolReq.AntiAffinityGroups = make([]v3.AntiAffinityGroup, l)
-		for i, v := range c.AntiAffinityGroups {
-			antiAffinityGroupList, err := client.ListAntiAffinityGroups(ctx)
-			if err != nil {
-				return err
-			}
-			aaG, err := antiAffinityGroupList.FindAntiAffinityGroup(v)
-			if err != nil {
-				return fmt.Errorf("error retrieving Anti-Affinity Group: %w", err)
-			}
-			nodepoolReq.AntiAffinityGroups[i] = aaG
-		}
-	}
-
-	if c.DeployTarget != "" {
-		deployTargetList, err := client.ListDeployTargets(ctx)
-		if err != nil {
-			return err
-		}
-		deployTarget, err := deployTargetList.FindDeployTarget(c.DeployTarget)
-		if err != nil {
-			return fmt.Errorf("error retrieving Deploy Target: %w", err)
-		}
-		nodepoolReq.DeployTarget = &deployTarget
-	}
-
-	nodepoolInstanceTypeList, err := client.ListInstanceTypes(ctx)
-	if err != nil {
-		return err
-	}
-	nodepoolInstanceType, err := nodepoolInstanceTypeList.FindInstanceTypeByIdOrFamilyAndSize(c.InstanceType)
-	if err != nil {
-		return fmt.Errorf("error retrieving instance type: %w", err)
-	}
-	nodepoolReq.InstanceType = &nodepoolInstanceType
-
-	if len(c.Labels) > 0 {
-		labels := make(map[string]string)
-		if len(c.Labels) > 0 {
-			labels, err = utils.SliceToMap(c.Labels)
-			if err != nil {
-				return fmt.Errorf("label: %w", err)
-			}
-		}
-		nodepoolReq.Labels = labels
-	}
-
-	if l := len(c.PrivateNetworks); l > 0 {
-		nodepoolPrivateNetworks := make([]v3.PrivateNetwork, l)
-		for i, v := range c.PrivateNetworks {
-			privateNetworksList, err := client.ListPrivateNetworks(ctx)
-			if err != nil {
-				return err
-			}
-			privateNetwork, err := privateNetworksList.FindPrivateNetwork(v)
-			if err != nil {
-				return fmt.Errorf("error retrieving Private Network: %w", err)
-			}
-			nodepoolPrivateNetworks[i] = privateNetwork
-		}
-		nodepoolReq.PrivateNetworks = nodepoolPrivateNetworks
-	}
-
-	if l := len(c.SecurityGroups); l > 0 {
-		nodepoolSecurityGroups := make([]v3.SecurityGroup, l)
-		for i, v := range c.SecurityGroups {
-			securityGroupList, err := client.ListSecurityGroups(ctx)
-			if err != nil {
-				return err
-			}
-			securityGroup, err := securityGroupList.FindSecurityGroup(v)
-			if err != nil {
-				return fmt.Errorf("error retrieving Security Group: %w", err)
-			}
-			nodepoolSecurityGroups[i] = securityGroup
-		}
-		nodepoolReq.SecurityGroups = nodepoolSecurityGroups
-	}
-
-	if len(c.Taints) > 0 {
-		taints := make(v3.SKSNodepoolTaints)
-		for _, t := range c.Taints {
-			key, taint, err := parseSKSNodepoolTaintV3(t)
-			if err != nil {
-				return fmt.Errorf("invalid taint value %q: %w", t, err)
-			}
-			taints[key] = *taint
-		}
-		nodepoolReq.Taints = taints
 	}
 
 	op, err := client.CreateSKSNodepool(ctx, cluster.ID, nodepoolReq)
