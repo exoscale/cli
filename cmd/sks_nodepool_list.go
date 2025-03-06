ef91e93390601e86c98003bd4da95858fadc27ba
Your Name <you@example.com>
2025-03-06 16:26:57 +0100

    list to v3.

--- cmd/sks_nodepool_list.go
+++ cmd/sks_nodepool_list.go
@@ -7,20 +7,19 @@ import (
 
 	"github.com/spf13/cobra"
 
-	"github.com/exoscale/cli/pkg/account"
 	"github.com/exoscale/cli/pkg/globalstate"
 	"github.com/exoscale/cli/pkg/output"
 	"github.com/exoscale/cli/utils"
-	exoapi "github.com/exoscale/egoscale/v2/api"
+	v3 "github.com/exoscale/egoscale/v3"
 )
 
 type sksNodepoolListItemOutput struct {
-	ID      string `json:"id"`
-	Name    string `json:"name"`
-	Cluster string `json:"cluster"`
-	Size    int64  `json:"size"`
-	State   string `json:"state"`
-	Zone    string `json:"zone"`
+	ID      v3.UUID     `json:"id"`
+	Name    string      `json:"name"`
+	Cluster string      `json:"cluster"`
+	Size    int64       `json:"size"`
+	State   string      `json:"state"`
+	Zone    v3.ZoneName `json:"zone"`
 }
 
 type sksNodepoolListOutput []sksNodepoolListItemOutput
@@ -34,7 +33,7 @@ type sksNodepoolListCmd struct {
 
 	_ bool `cli-cmd:"list"`
 
-	Zone string `cli-short:"z" cli-usage:"zone to filter results to"`
+	Zone v3.ZoneName `cli-short:"z" cli-usage:"zone to filter results to"`
 }
 
 func (c *sksNodepoolListCmd) cmdAliases() []string { return gListAlias }
@@ -53,12 +52,21 @@ func (c *sksNodepoolListCmd) cmdPreRun(cmd *cobra.Command, args []string) error 
 }
 
 func (c *sksNodepoolListCmd) cmdRun(_ *cobra.Command, _ []string) error {
-	var zones []string
+	client := globalstate.EgoscaleV3Client
+	ctx := gContext
+
+	resp, err := client.ListZones(ctx)
+	if err != nil {
+		return err
+	}
+	zones := resp.Zones
 
 	if c.Zone != "" {
-		zones = []string{c.Zone}
-	} else {
-		zones = utils.AllZones
+		endpoint, err := client.GetZoneAPIEndpoint(ctx, c.Zone)
+		if err != nil {
+			return err
+		}
+		zones = []v3.Zone{{APIEndpoint: endpoint}}
 	}
 
 	out := make(sksNodepoolListOutput, 0)
@@ -71,29 +79,30 @@ func (c *sksNodepoolListCmd) cmdRun(_ *cobra.Command, _ []string) error {
 		}
 		done <- struct{}{}
 	}()
-	err := utils.ForEachZone(zones, func(zone string) error {
-		ctx := exoapi.WithEndpoint(gContext, exoapi.NewReqEndpoint(account.CurrentAccount.Environment, zone))
+	err = utils.ForEveryZone(zones, func(zone v3.Zone) error {
 
-		list, err := globalstate.EgoscaleClient.ListSKSClusters(ctx, zone)
+		c := client.WithEndpoint((zone.APIEndpoint))
+		listResp, err := c.ListSKSClusters(ctx)
 		if err != nil {
 			return fmt.Errorf("unable to list SKS clusters in zone %s: %w", zone, err)
 		}
 
-		for _, cluster := range list {
+		for _, cluster := range listResp.SKSClusters {
 			for _, np := range cluster.Nodepools {
 				res <- sksNodepoolListItemOutput{
-					ID:      *np.ID,
-					Name:    *np.Name,
-					Cluster: *cluster.Name,
-					Size:    *np.Size,
-					State:   *np.State,
-					Zone:    zone,
+					ID:      np.ID,
+					Name:    np.Name,
+					Cluster: cluster.Name,
+					Size:    np.Size,
+					State:   string(np.State),
+					Zone:    zone.Name,
 				}
 			}
 		}
 
 		return nil
 	})
+
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr,
 			"warning: errors during listing, results might be incomplete.\n%s\n", err) // nolint:golint
