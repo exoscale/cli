aabc7dbf174d7c5cfe9bcc22aacc1bc5eb64dd8e
Your Name <you@example.com>
2025-05-05 18:43:04 +0200

    fixes.

--- cmd/sks_rotate_csi_credentials.go
+++ cmd/sks_rotate_csi_credentials.go
@@ -1,12 +1,10 @@
 package cmd
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
 
-	"github.com/exoscale/cli/pkg/account"
 	"github.com/exoscale/cli/pkg/globalstate"
 	v3 "github.com/exoscale/egoscale/v3"
 )
@@ -14,11 +12,10 @@ import (
 type sksRotateCSICredentialsCmd struct {
 	cliCommandSettings `cli-cmd:"-"`
 
-	_ bool `cli-cmd:"rotate-ccm-credentials"`
+	_ bool `cli-cmd:"rotate-csi-credentials"`
 
 	Cluster string `cli-arg:"#" cli-usage:"CLUSTER-NAME|ID"`
-
-	Zone v3.ZoneName `cli-flag:"zone" cli-short:"z" cli-usage:"SKS cluster zone"`
+	Zone 	v3.ZoneName `cli-flag:"zone" cli-short:"z" cli-usage:"SKS cluster zone"`
 }
 
 func (c *sksRotateCSICredentialsCmd) cmdAliases() []string { return nil }
@@ -50,7 +47,7 @@ func (c *sksRotateCSICredentialsCmd) cmdRun(_ *cobra.Command, _ []string) error 
 		return err
 	}
 
-	cluster, err := resp.FindSKSCluster(ctx, c.Zone, c.Cluster)
+	cluster, err := resp.FindSKSCluster(c.Cluster)
 	if err != nil {
 		return err
 	}
@@ -60,7 +57,7 @@ func (c *sksRotateCSICredentialsCmd) cmdRun(_ *cobra.Command, _ []string) error 
 		return err
 	}
 
-    decorateAsyncOperation(fmt.Sprintf("Rotating SKS cluster %q Exoscale CSI credentials...", c.Cluster), func() {
+	decorateAsyncOperation(fmt.Sprintf("Rotating SKS cluster %q Exoscale CSI credentials...", c.Cluster), func() {
 		_, err = client.Wait(ctx, op, v3.OperationStateSuccess)
 	})
 
