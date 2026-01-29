exoscale/cli

commit 9e128ed30a0a7267462e15517192b14df124f99f

do not switch client on listing entities per zone (#775)
# Description
Avoid redundant calls to `list-zones`

## Checklist
(For exoscale contributors)

* [x] Changelog updated (under *Unreleased* block)
* [x] Testing

## Testing

```
EXOSCALE_TRACE=1 go run main.go compute deploy-target list 2>&1 | grep Operation:
>>> Operation: list-zones
>>> Operation: list-deploy-targets
>>> Operation: list-deploy-targets
>>> Operation: list-deploy-targets
>>> Operation: list-deploy-targets

=======

EXOSCALE_TRACE=1 go run main.go compute elastic-ip list 2>&1 | grep Operation:
>>> Operation: list-zones
>>> Operation: list-elastic-ips
>>> Operation: list-elastic-ips
>>> Operation: list-elastic-ips
>>> Operation: list-elastic-ips

=======

EXOSCALE_TRACE=1 go run main.go compute instance list 2>&1 | grep Operation:
>>> Operation: list-zones
>>> Operation: list-instances
>>> Operation: list-instances
>>> Operation: list-instances
>>> Operation: list-instances
>>> Operation: get-instance-type
>>> Operation: get-instance-type

=======

EXOSCALE_TRACE=1 go run main.go compute instance snapshot list 2>&1 | grep Operation:
>>> Operation: list-zones
>>> Operation: list-snapshots
>>> Operation: list-snapshots
>>> Operation: list-snapshots
>>> Operation: list-snapshots
>>> Operation: get-instance

=======

EXOSCALE_TRACE=1 go run main.go compute instance list -z ch-gva-2 2>&1 | grep Operation:
>>> Operation: list-zones
>>> Operation: list-instances
>>> Operation: get-instance-type
>>> Operation: get-instance-type

=======

EXOSCALE_TRACE=1 go run main.go compute instance-pool list 2>&1 | grep Operation:
>>> Operation: list-zones
>>> Operation: list-instance-pools
>>> Operation: list-instance-pools
>>> Operation: list-instance-pools
>>> Operation: list-instance-pools

=======

EXOSCALE_TRACE=1 go run main.go compute load-balancer list 2>&1 | grep Operation:
>>> Operation: list-zones
>>> Operation: list-load-balancers
>>> Operation: list-load-balancers
>>> Operation: list-load-balancers

cmd/compute/instance/instance_snapshot_list.go
@@ -16,11 +16,11 @@ import (
 )
 
 type instanceSnapshotListItemOutput struct {
-	ID           string `json:"id"`
-	CreationDate string `json:"creation_date"`
-	Instance     string `json:"instance"`
-	State        string `json:"state"`
-	Zone         string `json:"zone"`
+	ID           v3.UUID     `json:"id"`
+	CreationDate string      `json:"creation_date"`
+	Instance     string      `json:"instance"`
+	State        string      `json:"state"`
+	Zone         v3.ZoneName `json:"zone"`
 }
 
 type instanceSnapshotListOutput []instanceSnapshotListItemOutput
@@ -34,7 +34,7 @@ type instanceSnapshotListCmd struct {
 
 	_ bool `cli-cmd:"list"`
 
-	Zone string `cli-short:"z" cli-usage:"zone to filter results to"`
+	Zone v3.ZoneName `cli-short:"z" cli-usage:"zone to filter results to"`
 }
 
 func (c *instanceSnapshotListCmd) CmdAliases() []string { return nil }
@@ -53,20 +53,12 @@ func (c *instanceSnapshotListCmd) CmdPreRun(cmd *cobra.Command, args []string) e
 }
 
 func (c *instanceSnapshotListCmd) CmdRun(_ *cobra.Command, _ []string) error {
-	var zones []v3.ZoneName
+	client := globalstate.EgoscaleV3Client
 	ctx := exocmd.GContext
 
-	if c.Zone != "" {
-		zones = []v3.ZoneName{v3.ZoneName(c.Zone)}
-	} else {
-		client, err := exocmd.SwitchClientZoneV3(ctx, globalstate.EgoscaleV3Client, v3.ZoneName(c.Zone))
-		if err != nil {
-			return err
-		}
-		zones, err = utils.AllZonesV3(ctx, client)
-		if err != nil {
-			return err
-		}
+	zones, err := utils.AllZonesV3(ctx, client, c.Zone)
+	if err != nil {
+		return err
 	}
 
 	out := make(instanceSnapshotListOutput, 0)
@@ -81,14 +73,9 @@ func (c *instanceSnapshotListCmd) CmdRun(_ *cobra.Command, _ []string) error {
 		}
 		done <- struct{}{}
 	}()
-	err := utils.ForEachZone(zones, func(zone v3.ZoneName) error {
-		ctx := exocmd.GContext
-		client, err := exocmd.SwitchClientZoneV3(ctx, globalstate.EgoscaleV3Client, v3.ZoneName(zone))
-		if err != nil {
-			return err
-		}
-
-		snapshots, err := client.ListSnapshots(ctx)
+	err = utils.ForEveryZone(zones, func(zone v3.Zone) error {
+		c := client.WithEndpoint(zone.APIEndpoint)
+		snapshots, err := c.ListSnapshots(ctx)
 		if err != nil {
 			return err
 		}
@@ -107,11 +94,11 @@ func (c *instanceSnapshotListCmd) CmdRun(_ *cobra.Command, _ []string) error {
 			}
 
 			res <- instanceSnapshotListItemOutput{
-				ID:           s.ID.String(),
+				ID:           s.ID,
 				CreationDate: s.CreatedAT.String(),
 				Instance:     instance.Name,
 				State:        string(s.State),
-				Zone:         string(zone),
+				Zone:         zone.Name,
 			}
 		}
 

cmd/compute/blockstorage/blockstorage_list.go
@@ -10,6 +10,7 @@ import (
 	exocmd "github.com/exoscale/cli/cmd"
 	"github.com/exoscale/cli/pkg/globalstate"
 	"github.com/exoscale/cli/pkg/output"
+	"github.com/exoscale/cli/utils"
 	v3 "github.com/exoscale/egoscale/v3"
 )
 
@@ -54,19 +55,9 @@ func (c *blockStorageListCmd) CmdRun(_ *cobra.Command, _ []string) error {
 	client := globalstate.EgoscaleV3Client
 	ctx := exocmd.GContext
 
-	resp, err := client.ListZones(ctx)
+	zones, err := utils.AllZonesV3(ctx, client, c.Zone)
 	if err != nil {
 		return err
-	}
-	zones := resp.Zones
-
-	if c.Zone != "" {
-		endpoint, err := client.GetZoneAPIEndpoint(ctx, c.Zone)
-		if err != nil {
-			return err
-		}
-
-		zones = []v3.Zone{{APIEndpoint: endpoint}}
 	}
 
 	output := make(blockStorageListOutput, 0)

cmd/compute/deploy_target/deploy_target_list.go
@@ -15,10 +15,10 @@ import (
 )
 
 type deployTargetListItemOutput struct {
-	Zone string `json:"zone"`
-	ID   string `json:"id"`
-	Name string `json:"name"`
-	Type string `json:"type"`
+	Zone v3.ZoneName `json:"zone"`
+	ID   v3.UUID     `json:"id"`
+	Name string      `json:"name"`
+	Type string      `json:"type"`
 }
 
 type deployTargetListOutput []deployTargetListItemOutput
@@ -32,7 +32,7 @@ type deployTargetListCmd struct {
 
 	_ bool `cli-cmd:"list"`
 
-	Zone string `cli-short:"z" cli-usage:"zone to filter results to"`
+	Zone v3.ZoneName `cli-short:"z" cli-usage:"zone to filter results to"`
 }
 
 func (c *deployTargetListCmd) CmdAliases() []string { return nil }
@@ -51,20 +51,12 @@ func (c *deployTargetListCmd) CmdPreRun(cmd *cobra.Command, args []string) error
 }
 
 func (c *deployTargetListCmd) CmdRun(_ *cobra.Command, _ []string) error {
-	var zones []v3.ZoneName
+	client := globalstate.EgoscaleV3Client
 	ctx := exocmd.GContext
 
-	if c.Zone != "" {
-		zones = []v3.ZoneName{v3.ZoneName(c.Zone)}
-	} else {
-		client, err := exocmd.SwitchClientZoneV3(ctx, globalstate.EgoscaleV3Client, v3.ZoneName(c.Zone))
-		if err != nil {
-			return err
-		}
-		zones, err = utils.AllZonesV3(ctx, client)
-		if err != nil {
-			return err
-		}
+	zones, err := utils.AllZonesV3(ctx, client, c.Zone)
+	if err != nil {
+		return err
 	}
 
 	out := make(deployTargetListOutput, 0)
@@ -77,24 +69,20 @@ func (c *deployTargetListCmd) CmdRun(_ *cobra.Command, _ []string) error {
 		}
 		done <- struct{}{}
 	}()
-	err := utils.ForEachZone(zones, func(zone v3.ZoneName) error {
 
-		client, err := exocmd.SwitchClientZoneV3(ctx, globalstate.EgoscaleV3Client, v3.ZoneName(zone))
-		if err != nil {
-			return err
-		}
-
-		list, err := client.ListDeployTargets(ctx)
+	err = utils.ForEveryZone(zones, func(zone v3.Zone) error {
+		c := client.WithEndpoint(zone.APIEndpoint)
+		list, err := c.ListDeployTargets(ctx)
 		if err != nil {
 			return fmt.Errorf("unable to list Deploy Targets in zone %s: %w", zone, err)
 		}
 
 		for _, dt := range list.DeployTargets {
 			res <- deployTargetListItemOutput{
-				ID:   dt.ID.String(),
+				ID:   dt.ID,
 				Name: dt.Name,
 				Type: string(dt.Type),
-				Zone: string(zone),
+				Zone: zone.Name,
 			}
 		}
 

cmd/compute/elastic_ip/elastic_ip_list.go
@@ -15,9 +15,9 @@ import (
 )
 
 type elasticIPListItemOutput struct {
-	ID        string `json:"id"`
-	IPAddress string `json:"ip_address"`
-	Zone      string `json:"zone"`
+	ID        v3.UUID     `json:"id"`
+	IPAddress string      `json:"ip_address"`
+	Zone      v3.ZoneName `json:"zone"`
 }
 
 type elasticIPListOutput []elasticIPListItemOutput
@@ -31,7 +31,7 @@ type elasticIPListCmd struct {
 
 	_ bool `cli-cmd:"list"`
 
-	Zone string `cli-short:"z" cli-usage:"zone to filter results to"`
+	Zone v3.ZoneName `cli-short:"z" cli-usage:"zone to filter results to"`
 }
 
 func (c *elasticIPListCmd) CmdAliases() []string { return exocmd.GListAlias }
@@ -50,20 +50,12 @@ func (c *elasticIPListCmd) CmdPreRun(cmd *cobra.Command, args []string) error {
 }
 
 func (c *elasticIPListCmd) CmdRun(_ *cobra.Command, _ []string) error {
-	var zones []v3.ZoneName
+	client := globalstate.EgoscaleV3Client
 	ctx := exocmd.GContext
 
-	if c.Zone != "" {
-		zones = []v3.ZoneName{v3.ZoneName(c.Zone)}
-	} else {
-		client, err := exocmd.SwitchClientZoneV3(ctx, globalstate.EgoscaleV3Client, v3.ZoneName(c.Zone))
-		if err != nil {
-			return err
-		}
-		zones, err = utils.AllZonesV3(ctx, client)
-		if err != nil {
-			return err
-		}
+	zones, err := utils.AllZonesV3(ctx, client, c.Zone)
+	if err != nil {
+		return err
 	}
 
 	out := make(elasticIPListOutput, 0)
@@ -76,13 +68,10 @@ func (c *elasticIPListCmd) CmdRun(_ *cobra.Command, _ []string) error {
 		}
 		done <- struct{}{}
 	}()
-	err := utils.ForEachZone(zones, func(zone v3.ZoneName) error {
-		client, err := exocmd.SwitchClientZoneV3(ctx, globalstate.EgoscaleV3Client, v3.ZoneName(zone))
-		if err != nil {
-			return err
-		}
+	err = utils.ForEveryZone(zones, func(zone v3.Zone) error {
+		c := client.WithEndpoint(zone.APIEndpoint)
+		list, err := c.ListElasticIPS(ctx)
 
-		list, err := client.ListElasticIPS(ctx)
 		if err != nil {
 			return fmt.Errorf("unable to list Elastic IP addresses in zone %s: %w", zone, err)
 		}
@@ -90,9 +79,9 @@ func (c *elasticIPListCmd) CmdRun(_ *cobra.Command, _ []string) error {
 		if list != nil {
 			for _, e := range list.ElasticIPS {
 				res <- elasticIPListItemOutput{
-					ID:        e.ID.String(),
+					ID:        e.ID,
 					IPAddress: e.IP,
-					Zone:      string(zone),
+					Zone:      zone.Name,
 				}
 			}
 

cmd/compute/instance/instance_list.go
@@ -17,13 +17,13 @@ import (
 )
 
 type instanceListItemOutput struct {
-	ID          string `json:"id"`
-	Name        string `json:"name"`
-	Zone        string `json:"zone"`
-	Type        string `json:"type"`
-	IPAddress   string `json:"ip_address"`
-	IPv6Address string `json:"ipv6_address"`
-	State       string `json:"state"`
+	ID          v3.UUID     `json:"id"`
+	Name        string      `json:"name"`
+	Zone        v3.ZoneName `json:"zone"`
+	Type        string      `json:"type"`
+	IPAddress   string      `json:"ip_address"`
+	IPv6Address string      `json:"ipv6_address"`
+	State       string      `json:"state"`
 }
 
 type instanceListOutput []instanceListItemOutput
@@ -46,9 +46,9 @@ func (o *instanceListOutput) ToTable() {
 
 	for _, instance := range *o {
 		t.Append([]string{
-			instance.ID,
+			string(instance.ID),
 			instance.Name,
-			instance.Zone,
+			string(instance.Zone),
 			instance.Type,
 			instance.IPAddress,
 			instance.IPv6Address,
@@ -62,7 +62,7 @@ type instanceListCmd struct {
 
 	_ bool `cli-cmd:"list"`
 
-	Zone string `cli-short:"z" cli-usage:"zone to filter results to"`
+	Zone v3.ZoneName `cli-short:"z" cli-usage:"zone to filter results to"`
 }
 
 func (c *instanceListCmd) CmdAliases() []string { return exocmd.GListAlias }
@@ -81,20 +81,12 @@ func (c *instanceListCmd) CmdPreRun(cmd *cobra.Command, args []string) error {
 }
 
 func (c *instanceListCmd) CmdRun(_ *cobra.Command, _ []string) error {
-	var zones []v3.ZoneName
+	client := globalstate.EgoscaleV3Client
 	ctx := exocmd.GContext
 
-	if c.Zone != "" {
-		zones = []v3.ZoneName{v3.ZoneName(c.Zone)}
-	} else {
-		client, err := exocmd.SwitchClientZoneV3(ctx, globalstate.EgoscaleV3Client, v3.ZoneName(c.Zone))
-		if err != nil {
-			return err
-		}
-		zones, err = utils.AllZonesV3(ctx, client)
-		if err != nil {
-			return err
-		}
+	zones, err := utils.AllZonesV3(ctx, client, c.Zone)
+	if err != nil {
+		return err
 	}
 
 	out := make(instanceListOutput, 0)
@@ -109,14 +101,11 @@ func (c *instanceListCmd) CmdRun(_ *cobra.Command, _ []string) error {
 		}
 		done <- struct{}{}
 	}()
-	err := utils.ForEachZone(zones, func(zone v3.ZoneName) error {
-		ctx := exocmd.GContext
-		client, err := exocmd.SwitchClientZoneV3(ctx, globalstate.EgoscaleV3Client, v3.ZoneName(zone))
-		if err != nil {
-			return err
-		}
+	err = utils.ForEveryZone(zones, func(zone v3.Zone) error {
 
-		instances, err := client.ListInstances(ctx)
+		c := client.WithEndpoint(zone.APIEndpoint)
+		instances, err := c.ListInstances(ctx)
+
 		if err != nil {
 			return err
 		}
@@ -138,9 +127,9 @@ func (c *instanceListCmd) CmdRun(_ *cobra.Command, _ []string) error {
 			}
 
 			res <- instanceListItemOutput{
-				ID:          i.ID.String(),
+				ID:          i.ID,
 				Name:        i.Name,
-				Zone:        string(zone),
+				Zone:        zone.Name,
 				Type:        fmt.Sprintf("%s.%s", instanceType.Family, instanceType.Size),
 				IPAddress:   utils.DefaultIP(&i.PublicIP, utils.EmptyIPAddressVisualization),
 				IPv6Address: utils.DefaultIP(i.Ipv6Address, utils.EmptyIPAddressVisualization),
@@ -150,6 +139,7 @@ func (c *instanceListCmd) CmdRun(_ *cobra.Command, _ []string) error {
 
 		return nil
 	})
+
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr,
 			"warning: errors during listing, results might be incomplete.\n%s\n", err) // nolint:golint

cmd/compute/instance_pool/instance_pool_list.go
@@ -15,11 +15,11 @@ import (
 )
 
 type instancePoolListItemOutput struct {
-	ID    string `json:"id"`
-	Name  string `json:"name"`
-	Zone  string `json:"zone"`
-	Size  int64  `json:"size"`
-	State string `json:"state"`
+	ID    v3.UUID     `json:"id"`
+	Name  string      `json:"name"`
+	Zone  v3.ZoneName `json:"zone"`
+	Size  int64       `json:"size"`
+	State string      `json:"state"`
 }
 
 type instancePoolListOutput []instancePoolListItemOutput
@@ -33,7 +33,7 @@ type instancePoolListCmd struct {
 
 	_ bool `cli-cmd:"list"`
 
-	Zone string `cli-short:"z" cli-usage:"zone to filter results to"`
+	Zone v3.ZoneName `cli-short:"z" cli-usage:"zone to filter results to"`
 }
 
 func (c *instancePoolListCmd) CmdAliases() []string { return exocmd.GListAlias }
@@ -52,20 +52,12 @@ func (c *instancePoolListCmd) CmdPreRun(cmd *cobra.Command, args []string) error
 }
 
 func (c *instancePoolListCmd) CmdRun(_ *cobra.Command, _ []string) error {
-	var zones []v3.ZoneName
+	client := globalstate.EgoscaleV3Client
 	ctx := exocmd.GContext
 
-	if c.Zone != "" {
-		zones = []v3.ZoneName{v3.ZoneName(c.Zone)}
-	} else {
-		client, err := exocmd.SwitchClientZoneV3(ctx, globalstate.EgoscaleV3Client, v3.ZoneName(c.Zone))
-		if err != nil {
-			return err
-		}
-		zones, err = utils.AllZonesV3(ctx, client)
-		if err != nil {
-			return err
-		}
+	zones, err := utils.AllZonesV3(ctx, client, c.Zone)
+	if err != nil {
+		return err
 	}
 
 	out := make(instancePoolListOutput, 0)
@@ -78,22 +70,19 @@ func (c *instancePoolListCmd) CmdRun(_ *cobra.Command, _ []string) error {
 		}
 		done <- struct{}{}
 	}()
-	err := utils.ForEachZone(zones, func(zone v3.ZoneName) error {
-		client, err := exocmd.SwitchClientZoneV3(ctx, globalstate.EgoscaleV3Client, zone)
-		if err != nil {
-			return err
-		}
+	err = utils.ForEveryZone(zones, func(zone v3.Zone) error {
+		c := client.WithEndpoint(zone.APIEndpoint)
+		list, err := c.ListInstancePools(ctx)
 
-		list, err := client.ListInstancePools(ctx)
 		if err != nil {
 			return fmt.Errorf("unable to list Instance Pools in zone %s: %w", zone, err)
 		}
 
 		for _, i := range list.InstancePools {
 			res <- instancePoolListItemOutput{
-				ID:    i.ID.String(),
+				ID:    i.ID,
 				Name:  i.Name,
-				Zone:  string(zone),
+				Zone:  zone.Name,
 				Size:  i.Size,
 				State: string(i.State),
 			}

cmd/compute/load_balancer/nlb_list.go
@@ -32,7 +32,7 @@ type nlbListCmd struct {
 
 	_ bool `cli-cmd:"list"`
 
-	Zone string `cli-short:"z" cli-usage:"zone to filter results to"`
+	Zone v3.ZoneName `cli-short:"z" cli-usage:"zone to filter results to"`
 }
 
 func (c *nlbListCmd) CmdAliases() []string { return exocmd.GListAlias }
@@ -51,20 +51,12 @@ func (c *nlbListCmd) CmdPreRun(cmd *cobra.Command, args []string) error {
 }
 
 func (c *nlbListCmd) CmdRun(_ *cobra.Command, _ []string) error {
-	var zones []v3.ZoneName
+	client := globalstate.EgoscaleV3Client
 	ctx := exocmd.GContext
 
-	if c.Zone != "" {
-		zones = []v3.ZoneName{v3.ZoneName(c.Zone)}
-	} else {
-		client, err := exocmd.SwitchClientZoneV3(ctx, globalstate.EgoscaleV3Client, v3.ZoneName(c.Zone))
-		if err != nil {
-			return err
-		}
-		zones, err = utils.AllZonesV3(ctx, client)
-		if err != nil {
-			return err
-		}
+	zones, err := utils.AllZonesV3(ctx, client, c.Zone)
+	if err != nil {
+		return err
 	}
 
 	out := make(nlbListOutput, 0)
@@ -77,13 +69,9 @@ func (c *nlbListCmd) CmdRun(_ *cobra.Command, _ []string) error {
 		}
 		done <- struct{}{}
 	}()
-	err := utils.ForEachZone(zones, func(zone v3.ZoneName) error {
-		client, err := exocmd.SwitchClientZoneV3(ctx, globalstate.EgoscaleV3Client, v3.ZoneName(zone))
-		if err != nil {
-			return err
-		}
-
-		list, err := client.ListLoadBalancers(ctx)
+	err = utils.ForEveryZone(zones, func(zone v3.Zone) error {
+		c := client.WithEndpoint(zone.APIEndpoint)
+		list, err := c.ListLoadBalancers(ctx)
 		if err != nil {
 			return fmt.Errorf("unable to list Network Load Balancers in zone %s: %w", zone, err)
 		}
@@ -92,7 +80,7 @@ func (c *nlbListCmd) CmdRun(_ *cobra.Command, _ []string) error {
 			res <- nlbListItemOutput{
 				ID:        nlb.ID,
 				Name:      nlb.Name,
-				Zone:      v3.ZoneName(zone),
+				Zone:      zone.Name,
 				IPAddress: nlb.IP.String(),
 			}
 		}

cmd/compute/private_network/private_network_list.go
@@ -53,18 +53,9 @@ func (c *privateNetworkListCmd) CmdRun(_ *cobra.Command, _ []string) error {
 	client := globalstate.EgoscaleV3Client
 	ctx := exocmd.GContext
 
-	resp, err := client.ListZones(ctx)
+	zones, err := utils.AllZonesV3(ctx, client, c.Zone)
 	if err != nil {
 		return err
-	}
-	zones := resp.Zones
-
-	if c.Zone != "" {
-		endpoint, err := client.GetZoneAPIEndpoint(ctx, c.Zone)
-		if err != nil {
-			return err
-		}
-		zones = []v3.Zone{{APIEndpoint: endpoint}}
 	}
 
 	out := make(privateNetworkListOutput, 0)
@@ -79,7 +70,7 @@ func (c *privateNetworkListCmd) CmdRun(_ *cobra.Command, _ []string) error {
 	}()
 	err = utils.ForEveryZone(zones, func(zone v3.Zone) error {
 
-		c := client.WithEndpoint((zone.APIEndpoint))
+		c := client.WithEndpoint(zone.APIEndpoint)
 		resp, err := c.ListPrivateNetworks(ctx)
 		if err != nil {
 			return fmt.Errorf("unable to list Private Networks in zone %s: %w", zone, err)

commit 2c7558756dd3ea41bcea1a87bd7f9daa3a7c5ccd

Fix incorrect zone handing in instance snapshot list (#787)
This is a fix for a bug when listing instance snapshots per zone,
introduced in 9e128ed.

cmd/compute/instance/instance_snapshot_list.go
@@ -86,7 +86,7 @@ func (c *instanceSnapshotListCmd) CmdRun(_ *cobra.Command, _ []string) error {
 			if cached {
 				instance = instanceI.(*v3.Instance)
 			} else {
-				instance, err = client.GetInstance(ctx, s.Instance.ID)
+				instance, err = c.GetInstance(ctx, s.Instance.ID)
 				if err != nil {
 					return fmt.Errorf("unable to retrieve Compute instance %q: %w", s.Instance.ID.String(), err)
 				}